db6525b4a87a0f47ffa9fc2d66c9f4d4acbb1afc
natew <natewienert@gmail.com>
2024-04-07 11:46:13 -1000

    feat(create-vxrn): add cli to bootstrap apps via "npm create vxrn"

---
+++ packages/create-vxrn/run.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const command = require.resolve('create-tamagui')
+const args = process.argv.slice(2)
+
+try {
+  require('child_process').execSync(`node ${command} ${args.join(' ')}`, {
+    stdio: 'inherit',
+  })
+} catch(err) {
+  process.exit(1)
+}
