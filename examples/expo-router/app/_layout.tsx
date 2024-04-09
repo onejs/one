c36be744d24c8a95d76fa72773d36b75967a8339
natew <natewienert@gmail.com>
2024-04-09 00:06:39 -1000

    fix: expo router native screen options

--- examples/expo-router/app/_layout.tsx
+++ examples/expo-router/app/_layout.tsx
@@ -1,13 +1,18 @@
+import { isWeb } from '@tamagui/core'
 import { Stack } from '@vxrn/expo-router'
 
 export default function Layout() {
   return (
     <Stack
-      screenOptions={{
-        header() {
-          return null
-        },
-      }}
+      screenOptions={
+        isWeb
+          ? {
+              header() {
+                return null
+              },
+            }
+          : {}
+      }
     />
   )
 }
