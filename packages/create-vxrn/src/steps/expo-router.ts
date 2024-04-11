2b872b9eec0b6e4565af7ae5cc02628823f56b38
szymonrybczak <szymon.rybczak@gmail.com>
2024-04-11 14:56:12 +0200

    chore: prettify logs

--- packages/create-vxrn/src/steps/expo-router.ts
+++ packages/create-vxrn/src/steps/expo-router.ts
@@ -6,23 +6,19 @@ import { exec } from '../helpers/exec'
 const packageManager = 'yarn'
 const useYarn = packageManager === 'yarn'
 
-const runCommand = (scriptName: string) =>
-  `${packageManager} ${useYarn ? '' : 'run '}${scriptName}`
+const runCommand = (scriptName: string) => `${packageManager} ${useYarn ? '' : 'run '}${scriptName}`
 
 const main: ExtraSteps = async ({ isFullClone, projectName }) => {
   exec(runCommand('patch-package'))
 
   if (isFullClone) {
     console.info(`
-  ${ansis.green.bold('Done!')} created a new project under ./${projectName}
-
-visit your project:
-  ${ansis.green('cd')} ${projectName}
+${ansis.green.bold('Done!')} Created a new project under ./${ansis.greenBright(projectName)} visit your project:
+ • ${ansis.green('cd')} ${projectName}
 `)
   }
   console.info(`
-  To start the dev server, run:
-    ${ansis.green(runCommand('dev'))}
+To start the dev server, run: ${ansis.green(runCommand('dev'))}
 `)
 }
 
