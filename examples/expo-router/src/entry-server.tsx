2453bdaebec1df6ef091c6117ece2ed191548051
natew <natewienert@gmail.com>
2024-04-14 13:57:21 -1000

    feat(expo-router): load routes and collect Helmet values on static render

--- examples/expo-router/src/entry-server.tsx
+++ examples/expo-router/src/entry-server.tsx
@@ -1,7 +1,23 @@
 import ReactDOMServer from 'react-dom/server'
 
-import { App } from './App'
+import { App, routes } from './App'
+import { preloadRoutes } from './hooks/useRoutes'
 
-export const render = ({ path }: { path: string }) => {
-  return ReactDOMServer.renderToString(<App />)
+export const render = async ({ path }: { path: string }) => {
+  await preloadRoutes(routes)
+
+  const collectedHead: { helmet?: Record<string, any> } = {}
+  globalThis['vxrn__headContext__'] = collectedHead
+
+  const appHtml = ReactDOMServer.renderToString(<App path={path} />)
+
+  const headHtml = `${Object.values(collectedHead?.helmet ?? {})
+    .map((v: any) => v.toString())
+    .join('\n')}`
+
+  return { appHtml, headHtml }
+}
+
+export async function getRoutes() {
+  return await preloadRoutes(routes)
 }
