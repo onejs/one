54916cb19de7125a6011ad32414a279bfd8b2837
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-15 22:40:23 +0330

    init

--- packages/vxrn/src/utils/getOptionsFilled.ts
+++ packages/vxrn/src/utils/getOptionsFilled.ts
@@ -1,13 +1,18 @@
 import { join } from 'node:path'
 import { readPackageJSON } from 'pkg-types'
+import { createRequire } from 'module'
 import FSExtra from 'fs-extra'
 import type { VXRNConfig } from '../types'
+
+const require = createRequire(import.meta.url)
 
 export type VXRNConfigFilled = Awaited<ReturnType<typeof getOptionsFilled>>
 
 export async function getOptionsFilled(options: VXRNConfig) {
   const { host = '127.0.0.1', root = process.cwd(), port = 8081 } = options
-  const packageRootDir = join(import.meta.url ?? __filename, '..', '..', '..').replace('file:', '')
+  
+  const packageRootDir = join(require.resolve('vxrn'), '../../..')
+
   const cacheDir = join(root, 'node_modules', '.cache', 'vxrn')
   const internalPatchesDir = join(packageRootDir, 'patches')
   const userPatchesDir = join(root, 'patches')
