c3abe489a10631cbff86e07a93d135cc64161867
natew <natewienert@gmail.com>
2024-04-20 02:50:37 -1000

    feat(viti): remove jiti dep and use vite to load config

--- packages/vxrn/src/utils/readVXRNConfig.ts
+++ packages/vxrn/src/utils/readVXRNConfig.ts
@@ -1,14 +1,22 @@
-import jiti from 'jiti'
 import FSExtra from 'fs-extra'
 import type { VXRNConfig } from '../types'
+import { createServer } from 'vite'
 
 export async function readVXRNConfig(): Promise<VXRNConfig> {
   if (!(await FSExtra.pathExists('vxrn.config.ts'))) {
     return {}
   }
-  const requireFile = jiti(process.cwd(), {
-    esmResolve: true,
+
+  // somewhat hacky creating a server just to read config?
+  const vite = await createServer({
+    appType: 'custom',
   })
-  const userConfig = requireFile('./vxrn.config.ts')
+
+  const userConfig = await vite.ssrLoadModule('./vxrn.config.ts', {
+    fixStacktrace: true,
+  })
+
+  await vite.close()
+
   return userConfig?.default ?? {}
 }
