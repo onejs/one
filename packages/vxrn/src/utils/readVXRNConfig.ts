1fcb2d6bcbcd4f11356b83bcde55c9e5030538b1
natew <natewienert@gmail.com>
2024-04-20 02:57:05 -1000

    fix(vxrn): fallback to jiti for cjs support

--- packages/vxrn/src/utils/readVXRNConfig.ts
+++ packages/vxrn/src/utils/readVXRNConfig.ts
@@ -1,22 +1,31 @@
 import FSExtra from 'fs-extra'
 import type { VXRNConfig } from '../types'
 import { createServer } from 'vite'
+import jiti from 'jiti'
 
 export async function readVXRNConfig(): Promise<VXRNConfig> {
   if (!(await FSExtra.pathExists('vxrn.config.ts'))) {
     return {}
   }
 
-  // somewhat hacky creating a server just to read config?
-  const vite = await createServer({
-    appType: 'custom',
-  })
+  // try esm load
+  try {
+    // somewhat hacky creating a server just to read config?
+    const vite = await createServer({
+      appType: 'custom',
+    })
 
-  const userConfig = await vite.ssrLoadModule('./vxrn.config.ts', {
-    fixStacktrace: true,
-  })
+    const userConfig = await vite.ssrLoadModule('./vxrn.config.ts', {
+      fixStacktrace: true,
+    })
 
-  await vite.close()
-
-  return userConfig?.default ?? {}
+    await vite.close()
+    return userConfig?.default ?? {}
+  } catch (err) {
+    const requireFile = jiti(process.cwd(), {
+      esmResolve: true,
+    })
+    const userConfig = requireFile('./vxrn.config.ts')
+    return userConfig?.default ?? {}
+  }
 }
