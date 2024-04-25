dff75d8a38eee8c1b725051f40567cb7e222a10b
natew <natewienert@gmail.com>
2024-04-25 07:05:46 -1000

    remove(examples-tamagui): remove tamagui example as router example includes it and reduces overhead

--- examples/tamagui/index.web.jsx
+++
@@ -1,6 +0,0 @@
-import './polyfill'
-import { createRoot } from 'react-dom/client'
-import { App } from './src/App'
-import React from 'react'
-
-createRoot(document.querySelector('#root')).render(<App />)
