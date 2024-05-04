bf26744a7f3fbf8483be516aef22fd89533d5321
natew <natewienert@gmail.com>
2024-05-04 13:18:12 -0400

    fix types

--- packages/vxrn/types/constants.d.ts
+++ packages/vxrn/types/constants.d.ts
@@ -1,8 +1,4 @@
 export declare const DEFAULT_PORT = 8081;
-<<<<<<< HEAD
-=======
-export declare const EMPTY_LOADER_STRING = "function loader() {return [][0 + 0]}";
->>>>>>> origin/main
 export declare const nativeExtensions: string[];
 export declare const webExtensions: string[];
 export declare const depsToOptimize: string[];
