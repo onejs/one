d7cdf7e8dc60d644f99b1b52ae84dc90220c268a
natew <natewienert@gmail.com>
2024-06-01 16:55:21 -1000

    forgot to save

--- packages/vxs/src/vite/vxs.ts
+++ packages/vxs/src/vite/vxs.ts
@@ -52,40 +52,6 @@ export function vxs(options: Options): PluginOption {
     // },
 
     {
-      name: 'remove-react-native-web-animated',
-      config() {
-        return {
-          optimizeDeps: {
-            esbuildOptions: {
-              plugins: [
-                {
-                  name: 'remove-react-native-web-animated',
-                  setup(build) {
-                    build.onResolve(
-                      {
-                        filter: /(react-native\/Animated\/Animated|PlatformPressable)$/,
-                      },
-                      (args) => {
-                        return { path: args.path, namespace: 'proxy-wormify' }
-                      }
-                    )
-
-                    build.onLoad({ filter: /.*/, namespace: 'proxy-wormify' }, (args) => {
-                      return {
-                        contents: `export * from "@tamagui/proxy-worm";`,
-                        loader: 'js',
-                      }
-                    })
-                  },
-                },
-              ],
-            },
-          },
-        }
-      },
-    },
-
-    {
       name: 'load-web-extensions',
       enforce: 'pre',
       async resolveId(id, importer = '') {
