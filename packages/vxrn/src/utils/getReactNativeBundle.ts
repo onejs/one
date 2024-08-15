c4036fc3948faffa97778e834fda6e19503ee0f3
Pokai Chang <mail@zeta.vg>
2024-08-16 04:01:24 +0800

    fix: ignore @supabase/node-fetch from RN bundle
    Fixes the `Error running module: "@supabase/node-fetch/lib/index.js”` error while using the Supabase SDK.

--- packages/vxrn/src/utils/getReactNativeBundle.ts
+++ packages/vxrn/src/utils/getReactNativeBundle.ts
@@ -67,6 +67,16 @@ export async function getReactNativeBundle(options: VXRNOptionsFilled, viteRNCli
           )
         }
 
+        let code = outputModule.code
+
+        // A hacky way to exclude node-fetch from the bundle.
+        //
+        // Some part of Supabase SDK will import node-fetch statically (https://github.com/supabase/supabase-js/blob/v2.45.1/src/lib/fetch.ts#L2), or dynamically (https://github.com/supabase/auth-js/blob/8222ee198a0ab10570e8b4c31ffb2aeafef86392/src/lib/helpers.ts#L99), causing the node-fetch to be included in the bundle, and while imported statically it will throw a runtime error when running on React Native.
+        if (outputModule.facadeModuleId?.includes('@supabase/node-fetch')) {
+          // This should be safe since the imported '@supabase/node-fetch' will not actually be used in Supabase SDK as there's already a global `fetch` in React Native.
+          code = ''
+        }
+
         return `
 // id: ${id}
 // name: ${outputModule.name}
@@ -76,7 +86,7 @@ ___vxrnAbsoluteToRelative___["${outputModule.facadeModuleId}"] = "${id}"
 ___modules___["${id}"] = ((exports, module) => {
 const require = createRequire("${id}", ${JSON.stringify(importsMap, null, 2)})
 
-${outputModule.code}
+${code}
 })
 
 ${
