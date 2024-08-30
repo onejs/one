697cc284e666c09fc1257037f33168053dc4976e
natew <natewienert@gmail.com>
2024-08-30 11:31:41 -1000

    feat(color-scheme): add color-scheme package with higher level color scheme utils

---
+++ packages/color-scheme/src/index.tsx
@@ -0,0 +1,102 @@
+import {
+  type ColorSchemeName,
+  setColorScheme,
+  useColorScheme as useColorSchemeBase,
+  useColorSchemeSetting,
+} from '@vxrn/universal-color-scheme'
+import { createContext, useContext, useLayoutEffect, useMemo } from 'react'
+
+export type Scheme = 'light' | 'dark'
+export type SchemeSetting = 'system' | 'light' | 'dark'
+
+export { getColorScheme, onColorSchemeChange } from '@vxrn/universal-color-scheme'
+
+const storageKey = 'vxrn-scheme'
+
+const getSetting = (): SchemeSetting =>
+  (typeof localStorage !== 'undefined' && (localStorage.getItem(storageKey) as SchemeSetting)) ||
+  'system'
+
+// on startup lets set from localstorage
+const setting = getSetting()
+
+if (setting !== 'system') {
+  setColorScheme(setting)
+}
+
+const SchemeContext = createContext<{
+  setting: SchemeSetting
+  value: 'light' | 'dark'
+}>({
+  setting: 'system',
+  value: 'light',
+})
+
+export const useColorScheme = () => {
+  const [state] = useColorSchemeBase()
+  return [state, setSchemeSetting] as const
+}
+
+export function useSchemeSetting() {
+  const values = useContext(SchemeContext)
+  return [values, setSchemeSetting] as const
+}
+
+export function setSchemeSetting(next: SchemeSetting) {
+  if (typeof localStorage !== 'undefined') {
+    localStorage.setItem(storageKey, next)
+  }
+  setColorScheme(next)
+}
+
+export function SchemeProvider({
+  children,
+  // defaults to tamagui-compatible
+  getClassName = (name) => `t_${name}`,
+}: { children: any; getClassName?: (name: ColorSchemeName) => string }) {
+  const [colorSchemeSetting] = useColorSchemeSetting()
+  const [colorScheme] = useColorScheme()
+
+  if (process.env.TAMAGUI_TARGET !== 'native') {
+    useLayoutEffect(() => {
+      const toAdd = getClassName(colorScheme)
+      const { classList } = document.documentElement
+      if (!classList.contains(toAdd)) {
+        const toRemove = colorScheme === 'light' ? 'dark' : 'light'
+        classList.remove(getClassName(toRemove))
+        classList.add(toAdd)
+      }
+    }, [colorScheme])
+  }
+
+  return (
+    <>
+      {process.env.TAMAGUI_TARGET === 'native' ? null : (
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `let d = document.documentElement.classList
+          d.remove('${getClassName('light')}')
+          d.remove('${getClassName('dark')}')
+          let e = localStorage.getItem('user-theme')
+          let t =
+            'system' === e || !e
+              ? window.matchMedia('(prefers-color-scheme: dark)').matches
+              : e === 'dark'
+          t ? d.add('${getClassName('dark')}') : d.add('${getClassName('light')}')`,
+          }}
+        />
+      )}
+      <SchemeContext.Provider
+        value={useMemo(
+          () => ({
+            value: colorScheme,
+            setting: colorSchemeSetting,
+          }),
+          [colorScheme, colorSchemeSetting]
+        )}
+      >
+        {children}
+      </SchemeContext.Provider>
+    </>
+  )
+}
---
+++ packages/color-scheme/types/index.d.ts
@@ -0,0 +1,15 @@
+import { type ColorSchemeName } from '@vxrn/universal-color-scheme';
+export type Scheme = 'light' | 'dark';
+export type SchemeSetting = 'system' | 'light' | 'dark';
+export { getColorScheme, onColorSchemeChange } from '@vxrn/universal-color-scheme';
+export declare const useColorScheme: () => readonly ["light" | "dark", typeof setSchemeSetting];
+export declare function useSchemeSetting(): readonly [{
+    setting: SchemeSetting;
+    value: "light" | "dark";
+}, typeof setSchemeSetting];
+export declare function setSchemeSetting(next: SchemeSetting): void;
+export declare function SchemeProvider({ children, getClassName, }: {
+    children: any;
+    getClassName?: (name: ColorSchemeName) => string;
+}): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=index.d.ts.map
