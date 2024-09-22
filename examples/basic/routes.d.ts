fed14bc7a88a14cd5cb29eab6286344a3ed2a437
natew <natewienert@gmail.com>
2024-09-22 09:02:55 -1000

    cleanup basic example

--- examples/basic/routes.d.ts
+++ examples/basic/routes.d.ts
@@ -1,11 +1,11 @@
-import type { VXSRouter } from 'vxs'
+import type { VXSRouter } from 'one'
 
-declare module 'vxs' {
+declare module 'one' {
   export namespace VXSRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(feed)` | `/(feed)/` | `/_sitemap` | `/notifications` | `/profile`
-      DynamicRoutes: `/(feed)/post/${VXSRouter.SingleRoutePart<T>}` | `/post/${VXSRouter.SingleRoutePart<T>}`
-      DynamicRouteTemplate: `/(feed)/post/[id]` | `/post/[id]`
+      StaticRoutes: `/` | `/(authenticated)` | `/(authenticated)/feed` | `/(authenticated)/feed/` | `/(authenticated)/notifications` | `/(authenticated)/profile` | `/(feed)` | `/(feed)/` | `/_sitemap` | `/feed` | `/feed/` | `/notifications` | `/profile`
+      DynamicRoutes: `/(authenticated)/feed/${VXSRouter.SingleRoutePart<T>}` | `/(feed)/post/${VXSRouter.SingleRoutePart<T>}` | `/feed/${VXSRouter.SingleRoutePart<T>}` | `/post/${VXSRouter.SingleRoutePart<T>}`
+      DynamicRouteTemplate: `/(authenticated)/feed/[id]` | `/(feed)/post/[id]` | `/feed/[id]` | `/post/[id]`
       IsTyped: true
     }
   }
