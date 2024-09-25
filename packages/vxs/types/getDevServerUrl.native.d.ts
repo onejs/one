67f64d0f482b4aef22163718860e5016fffc54ce
natew <natewienert@gmail.com>
2024-09-24 21:12:54 -1000

    v1.1.244

--- packages/vxs/types/getDevServerUrl.native.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * TODO: This is also used in production to get the server URL. We should probably split this into two functions to avoid confusion.
- */
-export declare function getDevServerUrl(): string;
-//# sourceMappingURL=getDevServerUrl.native.d.ts.map
