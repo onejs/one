607640e5ccf128a56dc2c5997ae8b11124805310
Pokai Chang <mail@zeta.vg>
2024-09-30 02:21:28 +0800

    perf: skip unnecessary built-in vite plugins for RN (#23)
    * perf: skip unnecessary built-in vite plugins for RN
    
    * limit to native

--- packages/vxrn/src/utils/fork/vite/build.ts
+++ packages/vxrn/src/utils/fork/vite/build.ts
@@ -40,6 +40,7 @@ import { displayTime } from './utils'
 import { getHookHandler } from './plugins'
 import type { RollupPluginHooks } from './typeUtils'
 import { ROLLUP_HOOKS } from './constants'
+import { isNativeEnvironment } from '../../environmentUtils'
 
 /**
  * Build an App environment, or a App library (if libraryOptions is provided)
@@ -57,6 +58,9 @@ export async function buildEnvironment(
     cache?: RollupCache | undefined
   } & (RollupOutput | RollupOutput[] | RollupWatcher)
 > {
+  // vxrn: isNative
+  const isNative = isNativeEnvironment(environment)
+
   const options = config.build
   const { logger } = environment
   const ssr = environment.name !== 'client'
@@ -126,7 +130,28 @@ export async function buildEnvironment(
         }
       }
 
-      return p;
+      // Filter out unnecessary `renderChunk` hooks that are not needed for RN
+      if (isNative && p.renderChunk) {
+        if (
+          [
+            'vite:build-metadata', // Inserts asset and CSS related metadata which is not needed for RN (see: https://github.com/vitejs/vite/blob/v6.0.0-alpha.18/packages/vite/src/node/plugins/metadata.ts#L10-L16)
+            'vite:worker', // See: https://github.com/vitejs/vite/blob/v6.0.0-alpha.18/packages/vite/src/node/plugins/worker.ts#L383-L429
+            'vite:asset', // Seems not needed since with RN we have our own asset handling logic (see: https://github.com/vitejs/vite/blob/v6.0.0-alpha.18/packages/vite/src/node/plugins/asset.ts#L214-L227)
+            'vite:css-post', // Seems not needed since we are not using CSS-in-JS for RN (see: https://github.com/vitejs/vite/blob/v6.0.0-alpha.18/packages/vite/src/node/plugins/css.ts#L560-L830)
+            'vite:force-systemjs-wrap-complete', // Not needed since we are not using SystemJS (see: https://github.com/vitejs/vite/blob/v6.0.0-alpha.18/packages/vite/src/node/plugins/completeSystemWrap.ts#L12-L21)
+            // 'vite:build-import-analysis',
+            'vite:esbuild-transpile', // Seems to be ok to remove.
+            'vite:reporter',
+          ].includes(p.name)
+        ) {
+          return {
+            ...p,
+            renderChunk: undefined,
+          }
+        }
+      }
+
+      return p
     }),
 
     external: options.rollupOptions?.external,
