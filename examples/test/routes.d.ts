5317dc5ef98580e9040a0194a5622d7f1f454c0e
natew <natewienert@gmail.com>
2024-10-02 19:40:33 -1000

    v1.1.277

--- examples/test/routes.d.ts
+++ examples/test/routes.d.ts
@@ -3,7 +3,7 @@ import type { OneRouter } from 'one'
 declare module 'one' {
   export namespace OneRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/_sitemap` | `/not-found/test` | `/ssr/basic`
+      StaticRoutes: `/` | `/_sitemap` | `/not-found/test` | `/ssr/basic` | `/sub-page` | `/sub-page/sub` | `/sub-page/sub2`
       DynamicRoutes: `/not-found/+not-found`
       DynamicRouteTemplate: `/not-found/+not-found`
       IsTyped: true
--- packages/query-string/types/index.d.ts
+++ packages/query-string/types/index.d.ts
@@ -1,4 +1,5 @@
 export declare const parse: (queryString: string) => Record<string, string | string[]>;
+export declare const stringify: (obj: Record<string, string | string[]>) => string;
 declare const _default: {
     parse: (queryString: string) => Record<string, string | string[]>;
     stringify: (obj: Record<string, string | string[]>) => string;
