1a563d3ff0cc3414403dd43860d7dfe2e385e763
natew <natewienert@gmail.com>
2024-10-02 22:10:30 -1000

    fix(one): use relative loader url on client in case SERVER_URL doesnt match

--- README.md
+++ README.md
@@ -1,31 +1,3 @@
-# vxrn
+# One
 
-[Documentation website](https://vxrn.dev)
-
-You can try the example apps:
-
-- `yarn dev:example-basic`
-
-## Contributing
-
-Install:
-
-```
-yarn install
-```
-
-Run watch in a separate terminal:
-
-```
-yarn watch
-```
-
-And then you can run the examples using `yarn dev:example-*`:
-
-```
-yarn dev:example-basic
-```
-
-### VSCode Extensions
-
-For auto formatting install [Biome](https://marketplace.visualstudio.com/items?itemName=biomejs.biome).
+See the [documentation website](https://one.dev).
--- packages/one/src/cleanUrl.ts
+++ packages/one/src/cleanUrl.ts
@@ -1,3 +1,4 @@
+import { isNative } from './constants'
 import { getURL } from './getURL'
 import { CACHE_KEY } from './router/constants'
 import { removeSearch } from './utils/removeSearch'
@@ -19,7 +20,11 @@ export function getPreloadPath(currentPath: string) {
   return `${clientSideURL}/assets/${cleanUrl(currentPath.slice(1))}_preload.js${clientSideSearch}`
 }
 
-export function getLoaderPath(currentPath: string, includeUrl = isClient) {
+export function getLoaderPath(
+  currentPath: string,
+  // browser can be relative
+  includeUrl = isNative
+) {
   const baseURL = includeUrl ? getURL() : ''
   const devPath = process.env.NODE_ENV === 'development' ? '/_one' : ''
   return `${baseURL}${devPath}/assets/${cleanUrl(currentPath.slice(1))}_vxrn_loader.js${clientSideSearch}`
