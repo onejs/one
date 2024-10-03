91c69d35e61b89647f487157b63b752e87258375
Pokai Chang <mail@zeta.vg>
2024-10-03 16:52:43 +0800

    [examples] assets: one logo (#109)

--- examples/one-recommended/code/brand/Logo.tsx
+++ examples/one-recommended/code/brand/Logo.tsx
@@ -1,5 +1,11 @@
-import { Circle } from 'tamagui'
+import { Image, styled } from 'tamagui'
+import oneBallImage from './one-ball.png'
+
+const LogoImage = styled(Image, {
+  width: 30,
+  height: 30,
+})
 
 export function Logo() {
-  return <Circle size={30} bg="$accentColor" />
+  return <LogoImage src={oneBallImage} />
 }
