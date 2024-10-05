154abeaa9338630b72b82c1dcddb6bcb2409fdcc
jonsherrard <jon@devular.com>
2024-10-05 17:45:43 +0100

    slim down basic

--- examples/one-basic/app/index.tsx
+++ examples/one-basic/app/index.tsx
@@ -1,17 +1,16 @@
 import { Text, View } from 'react-native'
-
 export default function Index() {
   return (
     <View
       style={{
-        width: 100,
-        height: 100,
+        flex: 1,
         alignSelf: 'center',
         justifyContent: 'center',
         alignItems: 'center',
+        minHeight: '100%',
       }}
     >
-      <Text>Hello world</Text>
+      <Text>Hello world, from One</Text>
     </View>
   )
 }
