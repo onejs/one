16da4c73b57fea2782fd6e456fd9dc2d571d2572
natew <natewienert@gmail.com>
2024-10-08 15:56:35 -1000

    remove one-zero demo app to bring back in better form

--- examples/one-zero/features/db/migrations/0001_brown_the_executioner.sql
+++
@@ -1,8 +0,0 @@
--- Custom SQL migration file, put you code below! --
-
-SELECT
-    *
-FROM
-    pg_create_logical_replication_slot('zero_slot_r1', 'pgoutput');
-
-VACUUM;
--- examples/one-zero/features/helpers/uuid.ts
+++
@@ -1,7 +0,0 @@
-export function uuid(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (char) => {
-    const random = (Math.random() * 16) | 0
-    const value = char === 'x' ? random : (random & 0x3) | 0x8
-    return value.toString(16)
-  })
-}
