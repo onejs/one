8ab3be87e292b291b01992b47df277ab1bd49a86
Pokai Chang <mail@zeta.vg>
2024-10-09 23:07:19 +0800

    testing auto optimizeDeps with `@tanstack/react-query`

--- examples/one-basic/app/index.tsx
+++ examples/one-basic/app/index.tsx
@@ -1,4 +1,8 @@
 import { Text, View } from 'react-native'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 export default function Index() {
   return (
     <View
@@ -11,6 +15,30 @@ export default function Index() {
       }}
     >
       <Text>Hello world, from One</Text>
+      <QueryClientProvider client={queryClient}>
+        <Example />
+      </QueryClientProvider>
+    </View>
+  )
+}
+
+function Example() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => fetch('https://api.github.com/repos/TanStack/query').then((res) => res.json()),
+  })
+
+  if (isPending) return <Text>Loading...</Text>
+
+  if (error) return <Text>{'An error has occurred: ' + error.message}</Text>
+
+  return (
+    <View>
+      <Text>{data.name}</Text>
+      <Text>{data.description}</Text>
+      <Text>👀 {data.subscribers_count}</Text>
+      <Text>✨ {data.stargazers_count}</Text>
+      <Text>🍴 {data.forks_count}</Text>
     </View>
   )
 }
--- examples/one-basic/vite.config.ts
+++ examples/one-basic/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import { one } from 'one/vite'
 
 export default defineConfig({
+  ssr: {
+    noExternal: true
+  },
   plugins: [
     one({
       web: {
