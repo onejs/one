694c91c0254508f3df2ec9c3ad1e125c9ecdafac
natew <natewienert@gmail.com>
2024-10-08 16:48:53 -1000

    fix(one): fix replacing minified loader when data is null, prevents outputting returnnull causing syntax error

--- packages/one/src/vite/replaceLoader.ts
+++ packages/one/src/vite/replaceLoader.ts
@@ -11,7 +11,13 @@ export function replaceLoader({
     if (!code.includes('__vxrn__loader__')) {
       return code + `\nexport const loader = () => (${stringifiedData})`
     }
-    return code.replace(/["']__vxrn__loader__['"]/, stringifiedData)
+    return code.replace(
+      /["']__vxrn__loader__['"]/,
+      // make sure this ' ' is added in front,
+      // minifiers will do `return"something"
+      // but if its null then it becomes returnnull
+      ' ' + stringifiedData
+    )
   })()
 
   return out
