5086ac713116168ca2201d7e8db88d5020208f52
natew <natewienert@gmail.com>
2024-10-14 12:48:14 +0900

    fallback to localhost still for empty getURL

--- packages/one/src/getURL.ts
+++ packages/one/src/getURL.ts
@@ -6,5 +6,5 @@ const CLIENT_BASE_URL =
 export function getURL() {
   // We should use `CLIENT_BASE_URL` on the client as we can't guarantee how the client is connected to the server - it's possible that the server is listening on `127.0.0.1` and the client is connected via `localhost`. Things might break if URLs don't match due to a JS module being double-loaded via two different URLs.
   // `process.env.ONE_SERVER_URL` is for the server, where `CLIENT_BASE_URL` will be blank.
-  return CLIENT_BASE_URL || process.env.ONE_SERVER_URL || ''
+  return CLIENT_BASE_URL || process.env.ONE_SERVER_URL || 'http://localhost'
 }
