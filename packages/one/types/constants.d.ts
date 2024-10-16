6862a0a55b6cb2ecf3c8d3eb589cf7d63dd82ddb
natew <natewienert@gmail.com>
2024-10-16 13:01:52 +0900

    fix and bring back db up check script

--- packages/one/types/constants.d.ts
+++ packages/one/types/constants.d.ts
@@ -1,4 +1,7 @@
 export declare const isWebClient: boolean;
 export declare const isWebServer: boolean;
 export declare const isNative: boolean;
+export declare const CACHE_KEY: string;
+export declare const LOADER_JS_POSTFIX: string;
+export declare const PRELOAD_JS_POSTFIX: string;
 //# sourceMappingURL=constants.d.ts.map
