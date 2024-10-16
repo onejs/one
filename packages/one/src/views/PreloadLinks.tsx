4db1585bbd13fb9d60513d0dd1931c7a0bf0f4d7
natew <natewienert@gmail.com>
2024-10-17 08:48:35 +0900

    chore(one): dispose on PreloadLinks

--- packages/one/src/views/PreloadLinks.tsx
+++ packages/one/src/views/PreloadLinks.tsx
@@ -5,20 +5,33 @@ import { getURL } from '../getURL'
 export function PreloadLinks() {
   // only in prod because we don't generate them until build
   // @ts-ignore
-  if (import.meta.env.PROD) {
+  if (typeof window !== 'undefined' && import.meta.env.PROD) {
     useEffect(() => {
       const url = getURL()
-      document.addEventListener('mouseover', (e) => {
-        let target = e.target
-        if (!(target instanceof HTMLElement)) return
-        target = target instanceof HTMLAnchorElement ? target : target.parentElement
-        if (!(target instanceof HTMLAnchorElement)) return
-        const href = target.getAttribute('href')
-        if (href?.[0] === '/' || href?.[0].startsWith(url)) {
-          // local route
-          preloadRoute(href.replace(url, ''))
+      const controller = new AbortController()
+
+      document.addEventListener(
+        'mouseover',
+        (e) => {
+          let target = e.target
+          if (!(target instanceof HTMLElement)) return
+          target = target instanceof HTMLAnchorElement ? target : target.parentElement
+          if (!(target instanceof HTMLAnchorElement)) return
+          const href = target.getAttribute('href')
+          if (href?.[0] === '/' || href?.[0].startsWith(url)) {
+            // local route
+            preloadRoute(href.replace(url, ''))
+          }
+        },
+        {
+          passive: true,
+          signal: controller.signal,
         }
-      })
+      )
+
+      return () => {
+        controller.abort()
+      }
     }, [])
   }
 
