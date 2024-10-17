1b938e05845d27f7372a740b331186338431fe5a
Pokai Chang <mail@zeta.vg>
2024-10-18 01:50:07 +0800

    test: print time info

--- packages/one/test/setup.ts
+++ packages/one/test/setup.ts
@@ -15,19 +15,21 @@ export type TestInfo = {
 const execAsync = promisify(exec)
 
 const waitForServer = (url: string, maxRetries = 30, retryInterval = 1000): Promise<void> => {
+  const startedAt = performance.now()
   return new Promise((resolve, reject) => {
     let retries = 0
     const checkServer = async () => {
       try {
         const response = await fetch(url)
         if (response.ok) {
+          console.info(`Server at ${url} is ready after ${Math.round(performance.now() - startedAt)}ms`)
           resolve()
         } else {
           throw new Error('Server not ready')
         }
       } catch (error) {
         if (retries >= maxRetries) {
-          reject(new Error(`Server at ${url} did not start within the expected time`))
+          reject(new Error(`Server at ${url} did not start within the expected time (timeout after waiting for ${Math.round(performance.now() - startedAt)}ms)`))
         } else {
           retries++
           setTimeout(checkServer, retryInterval)
@@ -54,6 +56,7 @@ export default async () => {
   try {
     // Run prod build using spawn
     console.info('Starting a prod build.')
+    const prodBuildStartedAt = performance.now()
     buildProcess = spawn('yarn', ['build:web'], {
       cwd: fixtureDir,
       env: {
@@ -66,10 +69,10 @@ export default async () => {
     await new Promise<void>((resolve, reject) => {
       buildProcess!.on('exit', (code) => {
         if (code === 0) {
-          console.info('Prod build completed successfully.')
+          console.info(`Prod build completed successfully after ${Math.round(performance.now() - prodBuildStartedAt)}ms`)
           resolve()
         } else {
-          reject(new Error(`Build process exited with code ${code}`))
+          reject(new Error(`Build process exited with code ${code} after ${Math.round(performance.now() - prodBuildStartedAt)}ms`))
         }
       })
       buildProcess!.on('error', reject)
