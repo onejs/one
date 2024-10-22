67293b396633f063fd298913b32fde32fedd7a7f
Pokai Chang <mail@zeta.vg>
2024-10-22 20:04:36 +0800

    basic setup of native integration test

--- tests/test/vite.config.ts
+++ tests/test/vite.config.ts
@@ -11,6 +11,14 @@ export default {
       app: {
         key: 'one-example',
       },
+
+      config: {
+        tsConfigPaths: {
+          // On CI, the mono-repo may be partially installed and built.
+          // While the `tsconfig-paths` plugin might attempt to parsing `tsconfig.json` other apps in the mono-repo and give us errors such as `failed to resolve "extends":"expo/tsconfig.base"`, we will ignore these errors.
+          ignoreConfigErrors: true,
+        },
+      },
     }),
   ],
 } satisfies UserConfig
