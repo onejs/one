b5fb1bfc9fb0df54b82d53435c1647b759eacfbc
natew <natewienert@gmail.com>
2024-10-28 12:26:35 +0900

    feat(one): add href() utility function to help creating typed Href strings

---
+++ packages/one/types/href.d.ts
@@ -0,0 +1,7 @@
+import type { OneRouter } from './interfaces/router';
+/**
+ * Type-level utility function to help pass valid Href typed strings.
+ * Does not actually validate at runtime, though we could add this later.
+ */
+export declare function href<A extends OneRouter.Href>(a: A): A;
+//# sourceMappingURL=href.d.ts.map
