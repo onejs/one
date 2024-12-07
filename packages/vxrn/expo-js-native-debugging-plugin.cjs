e0b0f87aa792d6401f70c3fa9a9b191a7e3ff179
Pokai Chang <mail@zeta.vg>
2024-12-07 17:33:24 +0800

    add JSI based RN JS bundle debugging setup

--- packages/vxrn/expo-js-native-debugging-plugin.cjs
+++ packages/vxrn/expo-js-native-debugging-plugin.cjs
@@ -22,44 +22,13 @@ const plugin = (config, options = {}) => {
       [
         'ios',
         async (config) => {
-          const { rctJsNativeDebuggingHFilePath, rctJsNativeDebuggingMFilePath } =
-            getIosFileNameAndPaths(config)
+          const srcPath = path.resolve(config.modRequest.projectRoot, config.modRequest.platformProjectRoot)
 
-          const rctJsNativeDebuggingHFileContent = `
-//
-//  RCTJsNativeDebugging.h
-//
-
-#import <React/RCTBridgeModule.h>
-@interface RCTJsNativeDebugging : NSObject <RCTBridgeModule>
-@end
-`.trim()
-          const rctJsNativeDebuggingMFileContent = `
-//
-//  RCTJsNativeDebugging.m
-//
-
-#import "RCTJsNativeDebugging.h"
-
-@implementation RCTJsNativeDebugging
-
-RCT_EXPORT_MODULE();
-
-// In JS: globalThis.nativeModuleProxy.JsNativeDebugging.log('message')
-RCT_EXPORT_METHOD(log:(NSString *)message)
-{
-  NSLog(@"%@", message);
-}
-
-@end
-`.trim()
-
-          if (!fs.existsSync(rctJsNativeDebuggingHFilePath)) {
-            fs.writeFileSync(rctJsNativeDebuggingHFilePath, rctJsNativeDebuggingHFileContent)
-          }
-
-          if (!fs.existsSync(rctJsNativeDebuggingMFilePath)) {
-            fs.writeFileSync(rctJsNativeDebuggingMFilePath, rctJsNativeDebuggingMFileContent)
+          for (const [fileName, fileContent] of Object.entries(IOS_FILES)) {
+            const filePath = path.resolve(srcPath, fileName)
+            if (!fs.existsSync(filePath)) {
+              fs.writeFileSync(filePath, fileContent)
+            }
           }
 
           return config
@@ -71,16 +40,16 @@ RCT_EXPORT_METHOD(log:(NSString *)message)
       async (config) => {
         const xcodeProject = config.modResults
 
-        const { rctJsNativeDebuggingHFileName, rctJsNativeDebuggingMFileName } =
-          getIosFileNameAndPaths(config)
-
         const librariesGroupKey = xcodeProject.findPBXGroupKey({ name: 'Libraries' })
 
-        if (!xcodeProject.hasFile(rctJsNativeDebuggingHFileName)) {
-          xcodeProject.addHeaderFile(rctJsNativeDebuggingHFileName, {}, librariesGroupKey)
-        }
-        if (!xcodeProject.hasFile(rctJsNativeDebuggingMFileName)) {
-          xcodeProject.addSourceFile(rctJsNativeDebuggingMFileName, {}, librariesGroupKey)
+        for (const fileName in IOS_FILES) {
+          if (!xcodeProject.hasFile(fileName)) {
+            if (fileName.endsWith('.h')) {
+              xcodeProject.addHeaderFile(fileName, {}, librariesGroupKey)
+            } else {
+              xcodeProject.addSourceFile(fileName, {}, librariesGroupKey)
+            }
+          }
         }
 
         return config
@@ -89,20 +58,177 @@ RCT_EXPORT_METHOD(log:(NSString *)message)
   ])
 }
 
-function getIosFileNameAndPaths(config) {
-  const srcPath = path.resolve(config.modRequest.projectRoot, config.modRequest.platformProjectRoot)
-  const rctJsNativeDebuggingHFileName = 'RCTJsNativeDebugging.h'
-  const rctJsNativeDebuggingMFileName = 'RCTJsNativeDebugging.m'
+const IOS_FILES = {
+  'RCTJsNativeDebugging.h': `
+//
+//  RCTJsNativeDebugging.h
+//
 
-  const rctJsNativeDebuggingHFilePath = path.resolve(srcPath, rctJsNativeDebuggingHFileName)
-  const rctJsNativeDebuggingMFilePath = path.resolve(srcPath, rctJsNativeDebuggingMFileName)
+#import <React/RCTBridgeModule.h>
+@interface RCTJsNativeDebugging : NSObject <RCTBridgeModule>
+@end
+  `.trim(),
+  'RCTJsNativeDebugging.m': `
+//
+//  RCTJsNativeDebugging.m
+//
 
-  return {
-    rctJsNativeDebuggingHFileName,
-    rctJsNativeDebuggingMFileName,
-    rctJsNativeDebuggingHFilePath,
-    rctJsNativeDebuggingMFilePath,
-  }
+#import "RCTJsNativeDebugging.h"
+
+@implementation RCTJsNativeDebugging
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(log:(NSString *)message)
+{
+  NSLog(@"%@", message);
+}
+
+@end
+  `.trim(),
+
+  'JSIDebuggingModule.h': `
+//
+//  JSIDebuggingModule.h
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface JSIDebuggingModule : NSObject <RCTBridgeModule>;
+
+@property (nonatomic, assign) BOOL setBridgeOnMainQueue;
+
+@end
+  `.trim(),
+
+  'JSIDebuggingModule.mm': `
+//
+//  JSIDebuggingModule.mm
+//
+
+#import "JSIDebuggingModule.h"
+#import <React/RCTBridge+Private.h>
+#import <React/RCTUtils.h>
+#import <jsi/jsi.h>
+#import "jsi_debugging.h"
+
+@implementation JSIDebuggingModule
+
+@synthesize bridge = _bridge;
+@synthesize methodQueue = _methodQueue;
+
+RCT_EXPORT_MODULE()
+
++ (BOOL)requiresMainQueueSetup {
+
+    return YES;
+}
+
+- (void)setBridge:(RCTBridge *)bridge {
+    _bridge = bridge;
+    _setBridgeOnMainQueue = RCTIsMainQueue();
+    [self installLibrary];
+}
+
+- (void)installLibrary {
+
+    RCTCxxBridge *cxxBridge = (RCTCxxBridge *)self.bridge;
+
+    if (cxxBridge.runtime) {
+      jsi_debugging::install(*(facebook::jsi::Runtime *)cxxBridge.runtime);
+    }
+}
+
+@end
+  `.trim(),
+
+  'jsi_debugging.h': `
+//
+//  jsi_debugging.h
+//
+
+#include <jsi/jsi.h>
+
+using namespace facebook::jsi;
+
+#ifndef JSIDebugging_h
+#define JSIDebugging_h
+
+namespace jsi_debugging {
+  void install(Runtime & jsiRuntime);
+}
+
+#endif /* JSIDebugging_h */
+  `.trim(),
+
+  'jsi_debugging.cpp': `
+//
+//  jsi_debugging.cpp
+//
+
+#include "jsi_debugging.h"
+#include <jsi/jsi.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace facebook::jsi;
+using namespace std;
+
+namespace jsi_debugging {
+    std::string jsiValueToCppString(facebook::jsi::Runtime& runtime, const facebook::jsi::Value& value) {
+        if (value.isString()) {
+            facebook::jsi::String jsiStr = value.asString(runtime);
+            return jsiStr.utf8(runtime);
+        } else if (value.isNumber()) {
+            return std::to_string(value.asNumber());
+        } else if (value.isBool()) {
+            return value.getBool() ? "true" : "false";
+        } else if (value.isObject()) {
+            facebook::jsi::Object obj = value.asObject(runtime);
+            if (obj.isFunction(runtime)) {
+                return "[Function]";
+            } else {
+                return "[Object]";
+            }
+        } else if (value.isNull()) {
+            return "null";
+        } else if (value.isUndefined()) {
+            return "undefined";
+        } else {
+            return "[Unknown Value]";
+        }
+    }
+
+    void install(Runtime & jsiRuntime) {
+        auto nativeLog = Function::createFromHostFunction(
+                                                          jsiRuntime,
+            PropNameID::forAscii(jsiRuntime, "nativeLog"),
+            1,
+            [](
+               facebook::jsi::Runtime& runtime,
+               const facebook::jsi::Value&,
+               const facebook::jsi::Value* args,
+                size_t count
+            ) {
+                std::ostringstream output;
+
+                for (size_t i = 0; i < count; i++) {
+                    if (i > 0) {
+                        output << " "; // Add space between arguments
+                    }
+                    output << jsiValueToCppString(runtime, args[i]);
+                }
+
+                std::cout << output.str() << std::endl;
+
+                return Value::undefined();
+            });
+
+        jsiRuntime.global().setProperty(jsiRuntime, "nativeLog", std::move(nativeLog));
+    }
+}
+  `.trim(),
 }
 
 module.exports = plugin
