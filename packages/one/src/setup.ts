37b8f5e68bd1baa910553e6f2aaf15747769f2ed
Pokai Chang <mail@zeta.vg>
2024-12-17 16:19:09 +0800

    temporally disable react-scan

--- packages/one/src/setup.ts
+++ packages/one/src/setup.ts
@@ -1,16 +1,17 @@
-import { scan } from 'react-scan'
+// FIXME: This will break the release build of the React Native app.
+// import { scan } from 'react-scan'
 
-if (process.env.ONE_ENABLE_REACT_SCAN) {
-  const val = JSON.parse(process.env.ONE_ENABLE_REACT_SCAN)
-  scan(
-    val === true
-      ? {
-          enabled: true,
-          showToolbar: false,
-        }
-      : val
-  )
-}
+// if (process.env.ONE_ENABLE_REACT_SCAN) {
+//   const val = JSON.parse(process.env.ONE_ENABLE_REACT_SCAN)
+//   scan(
+//     val === true
+//       ? {
+//           enabled: true,
+//           showToolbar: false,
+//         }
+//       : val
+//   )
+// }
 
 // fixes bad import error in expo-modules-core
 // without this you run into error loading web immediately
