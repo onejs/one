561f7f8f3035ccff30e2b6bd79f8c057907f34bc
Pokai Chang <mail@zeta.vg>
2024-12-20 12:08:11 +0800

    fix: expo-modules-core/build/createWebModule has been removed
    Ref: https://github.com/expo/expo/pull/29870

--- packages/one/src/setup.ts
+++ packages/one/src/setup.ts
@@ -16,4 +16,4 @@ if (process.env.ONE_ENABLE_REACT_SCAN) {
 // fixes bad import error in expo-modules-core
 // without this you run into error loading web immediately
 // where globalThis.expo is not defined
-import 'expo-modules-core/build/createWebModule'
+import 'expo-modules-core/src/web/index.web.ts'
