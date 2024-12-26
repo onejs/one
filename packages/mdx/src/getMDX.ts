6baf959816a12d90d3ff4959063547cdbb125f2f
Samuel Wall <me@samuelwall.co.uk>
2024-12-26 22:34:05 +0000

    fix: lint fail

--- packages/mdx/src/getMDX.ts
+++ packages/mdx/src/getMDX.ts
@@ -8,28 +8,28 @@ import { getHeadings } from './getHeadings'
 import type { Frontmatter, UnifiedPlugin } from './types'
 
 export async function getMDX(source: string, extraPlugins?: UnifiedPlugin) {
-    const {frontmatter, code} = await bundleMDX({
-      source,
-      mdxOptions(options) {
-        const plugins = [
-          ...(extraPlugins || []),
-          ...(options.rehypePlugins ?? []),
-          rehypeMetaAttribute,
-          rehypeHighlightCode,
-          rehypeAutolinkHeadings,
-          rehypeSlug,
-        ]
-        options.rehypePlugins = plugins as any
-        return options
-      },
-    })
+  const { frontmatter, code } = await bundleMDX({
+    source,
+    mdxOptions(options) {
+      const plugins = [
+        ...(extraPlugins || []),
+        ...(options.rehypePlugins ?? []),
+        rehypeMetaAttribute,
+        rehypeHighlightCode,
+        rehypeAutolinkHeadings,
+        rehypeSlug,
+      ]
+      options.rehypePlugins = plugins as any
+      return options
+    },
+  })
 
-    return {
-        frontmatter: {
-          ...frontmatter,
-          headings: getHeadings(source),
-          readingTime: readingTime(code),
-        } as Frontmatter,
-        code,
-      }
+  return {
+    frontmatter: {
+      ...frontmatter,
+      headings: getHeadings(source),
+      readingTime: readingTime(code),
+    } as Frontmatter,
+    code,
   }
+}
