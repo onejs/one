7b4d99afcd51ecb38e9b64be64ae49af9ffa78c0
natew <natewienert@gmail.com>
2024-12-30 11:21:05 -1000

    v1.1.376

--- packages/one/types/serve.d.ts
+++ packages/one/types/serve.d.ts
@@ -1,11 +1,11 @@
 import './polyfills-server';
 import type { VXRNOptions } from 'vxrn';
 export declare function serve(args?: VXRNOptions['server']): Promise<void | import("hono").Hono<import("hono/types").BlankEnv, import("hono/types").BlankSchema, "/"> | {
-    handler: (req: Request) => Response | Promise<Response>;
-    GET: (req: Request) => Response | Promise<Response>;
-    POST: (req: Request) => Response | Promise<Response>;
-    PATCH: (req: Request) => Response | Promise<Response>;
-    PUT: (req: Request) => Response | Promise<Response>;
-    OPTIONS: (req: Request) => Response | Promise<Response>;
+    handler: (req: Request, requestContext: import("hono/types").FetchEventLike) => Response | Promise<Response>;
+    GET: (req: Request, requestContext: import("hono/types").FetchEventLike) => Response | Promise<Response>;
+    POST: (req: Request, requestContext: import("hono/types").FetchEventLike) => Response | Promise<Response>;
+    PATCH: (req: Request, requestContext: import("hono/types").FetchEventLike) => Response | Promise<Response>;
+    PUT: (req: Request, requestContext: import("hono/types").FetchEventLike) => Response | Promise<Response>;
+    OPTIONS: (req: Request, requestContext: import("hono/types").FetchEventLike) => Response | Promise<Response>;
 }>;
 //# sourceMappingURL=serve.d.ts.map
