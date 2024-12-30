24699aa86bdeb0e72030fd03a8e33ad537c3a740
OlshaMB <michaelolshanskiy@icloud.com>
2024-12-30 21:33:27 +0300

    feat: Move out the generateForPlatform logic into a separate file, return the script replacement code and fixup some messages

--- packages/vxrn/src/exports/prebuild.ts
+++ packages/vxrn/src/exports/prebuild.ts
@@ -1,91 +1,9 @@
-import module from 'node:module'
 import path from 'node:path'
 import FSExtra from 'fs-extra'
 import { fillOptions } from '../utils/getOptionsFilled'
 import { applyBuiltInPatches } from '../utils/patches'
 import { detectPackageManager, PackageManagerName } from '@vxrn/utils'
-import { error, log } from 'node:console'
-/*
-This code block is partially copied from meta owned repos.
-Copyright (c) Facebook, Inc. and its affiliates.
-*/
-const generateForPlatform = async (root: string, platform: 'ios' | 'android', appConfig: any) => {
-  const dest = path.resolve(platform)
-  const appName = appConfig.name
-  const displayName = appConfig.displayName
-  const require = module.createRequire(root)
-  const importPath = require.resolve('@react-native-community/cli/build/tools/walk.js', {
-    paths: [root],
-  })
-  const src = path.join(
-    path.dirname(
-      require.resolve('@react-native-community/template/template/package.json', {
-        paths: [root],
-      })
-    ),
-    platform
-  )
-  const walk = (await import(importPath)).default.default
-  walk(src).forEach((absoluteSrc: string) => {
-    const relativeFilePath = transformPath(path.relative(src, absoluteSrc))
-      .replace(/HelloWorld/g, appName)
-      .replace(/helloworld/g, appName.toLowerCase())
-    const srcPath = absoluteSrc
-    const destPath = path.resolve(dest, relativeFilePath)
-    console.log('copying', '"' + srcPath + '"', 'to', '"' + destPath + '"')
-
-    const replacements: Record<string, string> = {
-      'Hello App Display Name': displayName || appName,
-      HelloWorld: appName,
-      helloworld: appName,
-    }
-
-    if (FSExtra.lstatSync(srcPath).isDirectory()) {
-      if (!FSExtra.existsSync(destPath)) {
-        FSExtra.mkdirSync(destPath)
-      }
-      return
-    }
-
-    const extension = path.extname(srcPath)
-
-    if (['.png', '.jar', '.keystore'].includes(extension)) {
-      FSExtra.copyFileSync(srcPath, destPath)
-      return
-    }
-    const srcPermissions = FSExtra.statSync(srcPath).mode
-    let content = FSExtra.readFileSync(srcPath, 'utf8')
-    Object.entries(replacements).forEach(([regex, value]) => {
-      content = content.replace(new RegExp(regex, 'g'), value)
-    })
-    FSExtra.writeFileSync(destPath, content, {
-      encoding: 'utf8',
-      mode: srcPermissions,
-    })
-  })
-}
-/*
-End of meta owned code
-End of copyright Facebook
-*/
-
-const transformPath = (filePath: string) => {
-  return filePath
-    .replace('_BUCK', 'BUCK')
-    .replace('_gitignore', '.gitignore')
-    .replace('_gitattributes', '.gitattributes')
-    .replace('_babelrc', '.babelrc')
-    .replace('_editorconfig', '.editorconfig')
-    .replace('_eslintrc.js', '.eslintrc.js')
-    .replace('_flowconfig', '.flowconfig')
-    .replace('_buckconfig', '.buckconfig')
-    .replace('_prettierrc.js', '.prettierrc.js')
-    .replace('_bundle', '.bundle')
-    .replace('_ruby-version', '.ruby-version')
-    .replace('_node-version', '.node-version')
-    .replace('_watchmanconfig', '.watchmanconfig')
-    .replace('_xcode.env', '.xcode.env')
-}
+import { generateForPlatform } from './prebuildWithoutExpo'
 
 export const prebuild = async ({
   root,
@@ -112,10 +30,17 @@ export const prebuild = async ({
         '--skip-dependency-update',
         'react,react-native,expo',
       ])
+      try {
+        const packageJsonPath = path.join(root, 'package.json')
+        let packageJsonContents = await FSExtra.readFile(packageJsonPath, 'utf8')
 
-      console.warn('expo unfortunatly added some scripts in package.json')
-      console.log('please replace "expo run:ios" with "one run:ios"')
-      console.log('please replace "expo run:android" with "one run:android"')
+        packageJsonContents = packageJsonContents.replace(/expo run:ios/g, 'one run:ios')
+        packageJsonContents = packageJsonContents.replace(/expo run:android/g, 'one run:android')
+
+        await FSExtra.writeFile(packageJsonPath, packageJsonContents, 'utf8')
+      } catch (error) {
+        console.error('Error updating package.json', error)
+      }
       // Remove the ios/.xcode.env.local file as it's causing problems `node: No such file or directory` during build
       try {
         FSExtra.removeSync(path.join(root, 'ios', '.xcode.env.local'))
@@ -125,6 +50,11 @@ export const prebuild = async ({
     } catch (e) {
       throw new Error(
         `Failed to prebuild native project: ${e}\nIs "expo" listed in your dependencies?`
+      )
+    }
+    if (!platform || platform === 'ios') {
+      console.info(
+        'Run `open ios/*.xcworkspace` in your terminal to open the prebuilt iOS project, then you can either run it via Xcode or archive it for distribution.'
       )
     }
   } else {
@@ -179,11 +109,12 @@ export const prebuild = async ({
         console.error('android/ folder already exists')
       }
     }
-  }
-  if (!platform || platform === 'ios') {
-    console.info(
-      'Run `open ios/*.xcworkspace` in your terminal to open the prebuilt iOS project, then you can either run it via Xcode or archive it for distribution.'
-    )
+    if (!platform || platform === 'ios') {
+      console.info(`Run cd ios && pod install`)
+      console.info(
+        'Then run `open *.xcworkspace` in your terminal to open the prebuilt iOS project, then you can either run it via Xcode or archive it for distribution.'
+      )
+    }
   }
   if (!platform || platform === 'android') {
     console.info(
---
+++ packages/vxrn/src/exports/prebuildWithoutExpo.ts
@@ -0,0 +1,84 @@
+import module from 'node:module'
+import path from 'node:path'
+import FSExtra from 'fs-extra'
+/*
+This code block is partially copied from meta owned repos.
+Copyright (c) Facebook, Inc. and its affiliates.
+*/
+export const generateForPlatform = async (
+  root: string,
+  platform: 'ios' | 'android',
+  appConfig: any
+) => {
+  const dest = path.resolve(platform)
+  const appName = appConfig.name
+  const displayName = appConfig.displayName
+  const require = module.createRequire(root)
+  const importPath = require.resolve('@react-native-community/cli/build/tools/walk.js', {
+    paths: [root],
+  })
+  const src = path.join(
+    path.dirname(
+      require.resolve('@react-native-community/template/template/package.json', {
+        paths: [root],
+      })
+    ),
+    platform
+  )
+  const walk = (await import(importPath)).default.default
+  walk(src).forEach((absoluteSrc: string) => {
+    const relativeFilePath = transformPath(path.relative(src, absoluteSrc))
+      .replace(/HelloWorld/g, appName)
+      .replace(/helloworld/g, appName.toLowerCase())
+    const srcPath = absoluteSrc
+    const destPath = path.resolve(dest, relativeFilePath)
+    console.log('copying', '"' + srcPath + '"', 'to', '"' + destPath + '"')
+
+    const replacements: Record<string, string> = {
+      'Hello App Display Name': displayName || appName,
+      HelloWorld: appName,
+      helloworld: appName,
+    }
+
+    if (FSExtra.lstatSync(srcPath).isDirectory()) {
+      if (!FSExtra.existsSync(destPath)) {
+        FSExtra.mkdirSync(destPath)
+      }
+      return
+    }
+
+    const extension = path.extname(srcPath)
+
+    if (['.png', '.jar', '.keystore'].includes(extension)) {
+      FSExtra.copyFileSync(srcPath, destPath)
+      return
+    }
+    const srcPermissions = FSExtra.statSync(srcPath).mode
+    let content = FSExtra.readFileSync(srcPath, 'utf8')
+    Object.entries(replacements).forEach(([regex, value]) => {
+      content = content.replace(new RegExp(regex, 'g'), value)
+    })
+    FSExtra.writeFileSync(destPath, content, {
+      encoding: 'utf8',
+      mode: srcPermissions,
+    })
+  })
+}
+
+const transformPath = (filePath: string) => {
+  return filePath
+    .replace('_BUCK', 'BUCK')
+    .replace('_gitignore', '.gitignore')
+    .replace('_gitattributes', '.gitattributes')
+    .replace('_babelrc', '.babelrc')
+    .replace('_editorconfig', '.editorconfig')
+    .replace('_eslintrc.js', '.eslintrc.js')
+    .replace('_flowconfig', '.flowconfig')
+    .replace('_buckconfig', '.buckconfig')
+    .replace('_prettierrc.js', '.prettierrc.js')
+    .replace('_bundle', '.bundle')
+    .replace('_ruby-version', '.ruby-version')
+    .replace('_node-version', '.node-version')
+    .replace('_watchmanconfig', '.watchmanconfig')
+    .replace('_xcode.env', '.xcode.env')
+}
