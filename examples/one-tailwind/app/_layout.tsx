d66415281387162a1311398f40965780c2d28d26
Dale <dellryuzi@gmail.com>
2025-01-03 18:47:56 +0700

    feat new one-tailwind example

---
+++ examples/one-tailwind/app/_layout.tsx
@@ -0,0 +1,24 @@
+import { Slot } from "one";
+import "./base.css";
+
+export default function Layout() {
+  return (
+    <>
+      {typeof document !== "undefined" && (
+        <>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=5"
+          />
+          <link rel="icon" href="/favicon.svg" />
+        </>
+      )}
+
+      <div className="bg-red-500 p-4 border uppercase">this is bg should be red by tailwind</div>
+
+      <Slot />
+    </>
+  );
+}
