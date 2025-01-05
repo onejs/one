58db77f8c5447fcbd9c3ecb1895c8ff6a745a3aa
Pokai Chang <mail@zeta.vg>
2025-01-06 01:57:50 +0800

    update Tamagui example

--- examples/one-tamagui/app/index.tsx
+++ examples/one-tamagui/app/index.tsx
@@ -1,13 +1,32 @@
 import { Image } from '@tamagui/image-next'
 import { Text, YStack } from 'tamagui'
+import { Link } from 'one'
 import { ToggleThemeButton } from '~/interface/ToggleThemeButton'
 import oneBall from '../public/app-icon.png'
 
 export function HomePage() {
   return (
-    <YStack bg="$color1" mih="100%" gap="$4" ai="center" jc="center" f={1}>
-      <Text fontSize={20}>Hello, world</Text>
+    <YStack bg="$color1" mih="100%" gap="$4" px="$4" ai="center" jc="center" f={1}>
+      <Text fontSize="$8" textAlign="center">
+        Hello, One.
+      </Text>
+
       <Image src={oneBall} width={128} height={128} />
+
+      <YStack gap="$2.5">
+        <Text fontSize="$5" textAlign="center" color="$gray11">
+          Edit <Text>app/index.tsx</Text> to change this screen and then come back to see
+          your edits.
+        </Text>
+        <Text fontSize="$5" textAlign="center" color="$gray11">
+          Read{' '}
+          <Link href="https://onestack.dev/docs/introduction">
+            <Text color="$yellow10">the docs</Text>
+          </Link>{' '}
+          to discover what to do next.
+        </Text>
+      </YStack>
+
       <ToggleThemeButton />
     </YStack>
   )
