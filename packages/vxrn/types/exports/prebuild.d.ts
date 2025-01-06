edc601ef74a35f99fb067b7a98ba3bde9a15fef5
natew <natewienert@gmail.com>
2025-01-06 14:47:30 -0800

    feat(vxrn): automatically adjust ios and android scripts in prebuild to account for monorepo relative paths

--- packages/vxrn/types/exports/prebuild.d.ts
+++ packages/vxrn/types/exports/prebuild.d.ts
@@ -3,4 +3,5 @@ export declare const prebuild: ({ root, platform, expo, }: {
     platform?: "ios" | "android" | string;
     expo: boolean;
 }) => Promise<void>;
+export declare function replaceInUTF8File(filePath: string, projectName: string, templateName: string): Promise<void>;
 //# sourceMappingURL=prebuild.d.ts.map
