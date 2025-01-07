cf46b52f24247c0a3a9be5a6745b3cb0339d9189
natew <natewienert@gmail.com>
2025-01-07 09:08:58 -1000

    v1.1.389

--- tests/test/app/ssr/[id]+ssr.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'one'
-import { Text } from 'react-native'
-
-export default () => {
-  const params = useParams<{ id: string }>()
-  return <Text>hello from ssr {params.id}</Text>
-}
