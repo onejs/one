bcaf0db1e67c9d35052fc1c9d0903cb7564366d3
Pokai Chang <mail@zeta.vg>
2025-01-10 06:29:06 +0800

    chore: add subpath example to test app

---
+++ tests/test/app/routes/subpath/[...subpath].tsx
@@ -0,0 +1,28 @@
+import { Link, Slot, usePathname } from 'one'
+import { View, Text } from 'tamagui'
+
+export async function generateStaticParams() {
+  return [{
+    subpath: '/routes/subpath/some-path'
+  }, {
+    subpath: '/routes/subpath/other-path'
+  }, {
+    subpath: '/routes/subpath/nested/path'
+  }]
+}
+
+export default function SubPathPage() {
+  return (
+    <View>
+      <Link href="/routes/subpath/some-path">
+        <Text>Go to some-path</Text>
+      </Link>
+      <Link href="/routes/subpath/other-path">
+        <Text>Go to other-path</Text>
+      </Link>
+      <Link href="/routes/subpath/nested/path">
+        <Text>Go to nested/path</Text>
+      </Link>
+    </View>
+  )
+}
