28cd32adb33d09ee4e1caa8ecc517c181f1c2096
Pokai Chang <mail@zeta.vg>
2025-01-09 20:16:44 +0800

    chore: repro tamagui/tamagui#3120 in test app

---
+++ tests/test/app/hooks/contents/_layout.tsx
@@ -0,0 +1,22 @@
+import { Link, Slot, usePathname } from 'one'
+import { View, Text } from 'tamagui'
+
+export default function HooksTestingLayout() {
+  const pathname = usePathname()
+  return (
+    <View>
+      <Text>Layout `usePathname()`: {pathname}</Text>
+      <Slot />
+
+      <Link href="/hooks/contents/page-1">
+        <Text>Go to page-1</Text>
+      </Link>
+      <Link href="/hooks/contents/page-2">
+        <Text>Go to page-2</Text>
+      </Link>
+      <Link href="/hooks">
+        <Text>Go to index</Text>
+      </Link>
+    </View>
+  )
+}
