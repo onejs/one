85038d3c60ac12c7fa70c42da4cdfaeae599f2a6
Pokai Chang <mail@zeta.vg>
2025-01-10 21:51:15 +0800

    fix: remove setting outdated `__vxrnLoaderProps__` and use server context instead

--- packages/one/src/constants.ts
+++ packages/one/src/constants.ts
@@ -1,3 +1,5 @@
+import type { ServerContext } from './utils/serverContext'
+
 export const isWebClient = process.env.TAMAGUI_TARGET !== 'native' && typeof window !== 'undefined'
 export const isWebServer = process.env.TAMAGUI_TARGET !== 'native' && typeof window === 'undefined'
 export const isNative = process.env.TAMAGUI_TARGET === 'native'
@@ -17,8 +19,10 @@ export const VIRTUAL_SSR_CSS_HREF = `/@id/__x00__${VIRTUAL_SSR_CSS_ENTRY}`
 
 export const SERVER_CONTEXT_KEY = '__one_server_context__'
 
-export const SPA_HEADER_ELEMENTS = `
+export const getSpaHeaderElements = ({
+  serverContext = {},
+}: { serverContext?: ServerContext } = {}) => `
   <script>globalThis['global'] = globalThis</script>
   <script>globalThis['__vxrnIsSPA'] = true</script>
-  <script>globalThis["${SERVER_CONTEXT_KEY}"] = {}</script>
+  <script>globalThis["${SERVER_CONTEXT_KEY}"] = ${JSON.stringify(serverContext)}</script>
 `
