e261e235093336deeea97693686f8753bf872791
natew <natewienert@gmail.com>
2025-01-10 11:12:29 -1000

    add simple test case

--- tests/test/app/index.tsx
+++ tests/test/app/index.tsx
@@ -1,15 +1,17 @@
 import { Link } from 'one'
-import { Text, View } from 'react-native'
 import { Button, H2, YStack } from 'tamagui'
 import { ToggleThemeButton } from '../features/theme/ToggleThemeButton'
 
 export default () => (
-  <YStack f={1} ai="center" jc="center" gap="$6">
+  <YStack h={600} bg="red" f={1} ai="center" jc="center" gap="$10">
     <H2>Welcome to One</H2>
 
     <Link asChild href="/sub-page/sub">
-      <Button>Go to sub</Button>
+      <Button size="$5" id="go-to-sub">
+        Go to sub
+      </Button>
     </Link>
+
     <Link asChild href="/sheet">
       <Button>Open Sheet</Button>
     </Link>
--- tests/test/app/sub-page/sub.tsx
+++ tests/test/app/sub-page/sub.tsx
@@ -1,3 +1,4 @@
+import { Button, Dialog, H1, Paragraph, Sheet, YStack } from 'tamagui'
 import { Logo } from '~/features/brand/Logo'
 import { testData } from '~/features/feed/testData'
 import { ToggleThemeButton } from '~/features/theme/ToggleThemeButton'
@@ -11,6 +12,43 @@ export default function Test() {
     <>
       <Logo />
       <ToggleThemeButton />
+
+      <YStack id="test-sub-box" w={500} h={500} bg="$color10">
+        <Paragraph size="$4">Test Sub Sub</Paragraph>
+      </YStack>
+
+      <DialogTest />
     </>
+  )
+}
+
+export const DialogTest = (props) => {
+  return (
+    <Dialog modal open={props.show}>
+      <Dialog.Trigger>
+        <Button>Open</Button>
+      </Dialog.Trigger>
+
+      <Dialog.Adapt when="sm">
+        <Sheet zIndex={200000} modal dismissOnSnapToBottom animation="medium">
+          <Sheet.Frame bg="$color2" padding={0} gap="$4">
+            <Sheet.ScrollView>
+              <Dialog.Adapt.Contents />
+            </Sheet.ScrollView>
+          </Sheet.Frame>
+          <Sheet.Overlay animation="lazy" enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
+        </Sheet>
+      </Dialog.Adapt>
+
+      <Dialog.Portal>
+        <Dialog.Overlay key="overlay" />
+
+        <Dialog.Content key="content">
+          <YStack bg="$red10" borderWidth={20} borderColor="$green10" w={350} h={350}>
+            <H1>ok ok</H1>
+          </YStack>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
   )
 }
