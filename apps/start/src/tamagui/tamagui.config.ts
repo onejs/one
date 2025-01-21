2344d40f7ce82fc34c95d39cb2e6ba8c00e82274
natew <natewienert@gmail.com>
2025-01-20 19:56:50 -1000

    upgrade tamagui

--- apps/start/src/tamagui/tamagui.config.ts
+++ apps/start/src/tamagui/tamagui.config.ts
@@ -1,9 +1,11 @@
 import { defaultConfig, tamaguiThemes } from '@tamagui/config/v4'
 import { createTamagui } from 'tamagui'
+import { fonts } from './fonts'
 
 export const config = createTamagui({
   ...defaultConfig,
   themes: tamaguiThemes,
+  fonts,
 })
 
 export type Conf = typeof config
