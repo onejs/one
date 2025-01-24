a9efc197578300d22dc088d0ef5146eabf2e1d6d
natew <natewienert@gmail.com>
2025-01-23 18:21:00 -1000

    fix type

--- packages/one/types/vite/one-server-only.native.d.ts
+++ packages/one/types/vite/one-server-only.native.d.ts
@@ -5,4 +5,5 @@ export declare function mergeHeaders(onto: any, from: any): void;
 export declare function setServerContext(): void;
 export declare function setServerData(): void;
 export declare function useServerContext(): void;
+export declare function getServerData(): void;
 //# sourceMappingURL=one-server-only.native.d.ts.map
