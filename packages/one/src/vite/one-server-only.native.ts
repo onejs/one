a3d3fb895473193eb4a06916a6b64e101dccd9b1
natew <natewienert@gmail.com>
2025-01-23 15:29:21 -1000

    fix(one): another missing export

--- packages/one/src/vite/one-server-only.native.ts
+++ packages/one/src/vite/one-server-only.native.ts
@@ -5,3 +5,4 @@ export function mergeHeaders(onto, from) {}
 export function setServerContext() {}
 export function setServerData() {}
 export function useServerContext() {}
+export function getServerData() {}
