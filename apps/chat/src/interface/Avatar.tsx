11a492fc67bc1dc34dd032f540117c44f3b947f8
natew <natewienert@gmail.com>
2025-01-27 17:22:07 -1000

    fix sst db access

--- apps/chat/src/interface/Avatar.tsx
+++ apps/chat/src/interface/Avatar.tsx
@@ -43,7 +43,7 @@ export const Avatar = ({
       overflow="hidden"
       {...rest}
     >
-      {image && <Image src={image} width={size} height={size} />}
+      {image ? <Image src={image} width={size} height={size} /> : null}
     </SelectableCircle>
   )
 }
