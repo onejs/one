8b1abad46deb65ffc1510dbc4c6be7712c2efc12
natew <natewienert@gmail.com>
2025-01-29 06:22:09 -1000

    add some basic chat features

--- apps/chat/app/api/auth/[...sub]+api.ts
+++ apps/chat/app/api/auth/[...sub]+api.ts
@@ -3,19 +3,29 @@ import { auth } from '~/better-auth/auth'
 import { prettyPrintResponse } from '~/helpers/prettyPrintResponse'
 
 export const GET: Endpoint = async (req) => {
-  const res = await auth.handler(req)
-  if (process.env.DEBUG) {
-    console.info('AUTH GET', req.url)
-    prettyPrintResponse(res)
+  try {
+    const res = await auth.handler(req)
+    if (process.env.DEBUG) {
+      console.info('AUTH GET', req.url)
+      prettyPrintResponse(res)
+    }
+    return res
+  } catch (err) {
+    console.error(`Error: ${err}`)
+    return new Response(`Error`, { status: 500 })
   }
-  return res
 }
 
 export const POST: Endpoint = async (req) => {
-  const res = await auth.handler(req)
-  if (process.env.DEBUG) {
-    console.info('AUTH POST', req.url)
-    prettyPrintResponse(res)
+  try {
+    const res = await auth.handler(req)
+    if (process.env.DEBUG) {
+      console.info('AUTH POST', req.url)
+      prettyPrintResponse(res)
+    }
+    return res
+  } catch (err) {
+    console.error(`Error: ${err}`)
+    return new Response(`Error`, { status: 500 })
   }
-  return res
 }
