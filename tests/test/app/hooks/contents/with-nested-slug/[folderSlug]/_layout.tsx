61d07b0da710405c5a65cc74a641da9cd3f2bd34
Pokai Chang <mail@zeta.vg>
2025-03-10 00:54:31 +0800

    test: update hooks test app

---
+++ tests/test/app/hooks/contents/with-nested-slug/[folderSlug]/_layout.tsx
@@ -0,0 +1,22 @@
+import { Slot, useParams, usePathname } from 'one'
+import { View, Text, HR } from 'tamagui'
+
+export default function HooksTestingSlugLayout() {
+  const pathname = usePathname()
+  const params = useParams()
+
+  return (
+    <View>
+      <Text id="slug-layout-usePathname">
+        Slug layout `usePathname()`:{' '}
+        <Text testID="slug-layout-usePathname">{pathname}</Text>
+      </Text>
+      <Text id="slug-layout-useParams">
+        Slug layout `useParams()`:{' '}
+        <Text testID="slug-layout-useParams">{JSON.stringify(params)}</Text>
+      </Text>
+
+      <Slot />
+    </View>
+  )
+}
---
+++ tests/test/features/hooks-testing/HooksTestingLinks.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'one'
+import { View, Text } from 'tamagui'
+
+export function HooksTestingLinks() {
+  return (
+    <View>
+      <Link href="/hooks/contents/page-1">
+        <Text>Go to page-1</Text>
+      </Link>
+      <Link href="/hooks/contents/page-2">
+        <Text>Go to page-2</Text>
+      </Link>
+
+      <Link href="/hooks/contents/with-slug/slug-page-foo">
+        <Text>Go to with-slug/slug-page-foo</Text>
+      </Link>
+      <Link href="/hooks/contents/with-slug/slug-page-bar">
+        <Text>Go to with-slug/slug-page-bar</Text>
+      </Link>
+
+      <Link href="/hooks/contents/with-nested-slug/foo/bar">
+        <Text>Go to with-nested-slug/foo/bar</Text>
+      </Link>
+      <Link href="/hooks/contents/with-nested-slug/abc/def">
+        <Text>Go to with-nested-slug/abc/def</Text>
+      </Link>
+
+      <Link href="/hooks">
+        <Text>Go to index</Text>
+      </Link>
+    </View>
+  )
+}
