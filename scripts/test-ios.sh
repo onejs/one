3e3ab0076e3986de9a807f76144c8e8b12633164
Pokai Chang <mail@zeta.vg>
2025-03-15 02:24:01 +0800

    add scripts/test-ios.sh that prepares the env and runs all ios tests

---
+++ scripts/test-ios.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+set -e
+
+cd "$(dirname "$0")/.."
+
+tests/rn-test-container/scripts/build-ios.sh Debug
+tests/rn-test-container/scripts/build-ios.sh Release
+
+export DEV_PORT=8081 # currently we cannot dynamically set the port for the test container app, so we need to force it to be 8081
+
+export IOS_TEST_CONTAINER_PATH_DEV="$(tests/rn-test-container/scripts/get-ios-built-app-path.sh Debug)"
+export IOS_TEST_CONTAINER_PATH_PROD="$(tests/rn-test-container/scripts/get-ios-built-app-path.sh Release)"
+
+scripts/install-and-run-appium.sh
+
+yarn test-ios:run
