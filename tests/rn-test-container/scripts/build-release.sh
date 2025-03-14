19c6479b658d2cdd0e99b2caee541e86a9442d45
Pokai Chang <mail@zeta.vg>
2025-03-14 15:47:16 +0800

    ensure pod install has been run correctly

--- tests/rn-test-container/scripts/build-release.sh
+++ tests/rn-test-container/scripts/build-release.sh
@@ -4,6 +4,7 @@ set -o pipefail # Since we pipe the output to xcpretty, we need this to fail thi
 
 # yarn prebuild:native --platform ios --no-install # --no-install is used to skip installing dependencies, specifically `pod install` as we want to do it after the Cache Pods step
 yarn prebuild:native --platform ios
+pod install --project-directory=ios # do this again since expo is somehow reliable on running `pod install` during prebuild when prebuild has already been run before
 
 xcrun xcodebuild -scheme 'RNTestContainer' \
   -workspace "ios/RNTestContainer.xcworkspace" \
