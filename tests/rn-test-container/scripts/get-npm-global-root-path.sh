0150419876f79a16e7cdd979a789380bca6c432a
Pokai Chang <mail@zeta.vg>
2025-03-15 02:25:00 +0800

    remove unused scripts

--- tests/rn-test-container/scripts/get-npm-global-root-path.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-set -x
-set -e
-set -o pipefail # Since we pipe the output to xcpretty, we need this to fail this step if `xcrun xcodebuild` fails
-
-NPM_GLOBAL_ROOT_PATH=$(npm root -g)
-echo "NPM global root path: $NPM_GLOBAL_ROOT_PATH"
-# echo "path=$NPM_GLOBAL_ROOT_PATH" >> $GITHUB_OUTPUT
