a36c88bf64f1b1d43917bbed7c3c20d382666ec4
Pokai Chang <mail@zeta.vg>
2025-03-16 23:29:13 +0800

    fix(getPathFromState): handle nested params for nested navs

--- packages/one/src/fork/getPathFromState.ts
+++ packages/one/src/fork/getPathFromState.ts
@@ -233,7 +233,7 @@ export function getPathDataFromState<ParamList extends {}>(
               : undefined
 
         if (screen && screens && currentOptions[route.name].screens?.[screen]) {
-          route = { ...screens[screen], name: screen, key: screen }
+          route = { ...screens[screen], name: screen, key: screen, params: (route.params as any)?.params }
           currentOptions = screens
         } else {
           hasNext = false
