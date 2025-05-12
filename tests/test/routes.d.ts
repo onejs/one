e253891c79c781cd15cc44ccb5620ff5813892cb
natew <natewienert@gmail.com>
2025-05-12 07:30:56 -1000

    v1.1.484

--- tests/test/routes.d.ts
+++ tests/test/routes.d.ts
@@ -6,10 +6,10 @@ import type { OneRouter } from 'one'
 declare module 'one' {
   export namespace OneRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: unknown
-      DynamicRoutes: unknown
-      DynamicRouteTemplate: unknown
-      IsTyped: false
+      StaticRoutes: `/` | `/_sitemap`
+      DynamicRoutes: never
+      DynamicRouteTemplate: never
+      IsTyped: true
     }
   }
 }
