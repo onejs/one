029117ca50b94743ac3c901aeed96cc5bd1b9fb5
Pokai Chang <mail@zeta.vg>
2025-05-15 01:01:36 +0800

    test: add test to repro catch-all routes breaking public files and assets on prod

---
+++ tests/test/app-cases/catch-all-route-at-root-with-assets/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { Image } from '@tamagui/image-next'
+import sampleAssetPng from '../sample-asset.png'
+
+export default function Index() {
+  return (
+    <View>
+      <Image testID="sample-asset-png" src={sampleAssetPng} width={128} height={128} />
+      <Image testID="sample-public-png" src="/app-icon.png" width={128} height={128} />
+    </View>
+  )
+}
