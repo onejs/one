c9979fc6796bc5fabebdcfdf9266c737e2da949c
Pokai Chang <mail@zeta.vg>
2025-05-27 13:45:15 +0800

    fix(examples/one-zero): define necessary permissions
    Access is now denied by default: https://zero.rocicorp.dev/docs/permissions#access-is-denied-by-default

--- examples/one-zero/src/zero/schema.ts
+++ examples/one-zero/src/zero/schema.ts
@@ -1,8 +1,11 @@
 import {
+  ANYONE_CAN,
   createSchema,
   definePermissions,
+  type ExpressionBuilder,
   json,
   number,
+  type PermissionsConfig,
   type Row,
   string,
   table,
@@ -39,6 +42,43 @@ export type Schema = typeof schema
 export type Message = Row<Schema['tables']['message']>
 export type User = Row<Schema['tables']['user']>
 
-export const permissions = definePermissions<{}, Schema>(schema, () => {
-  return {}
+// The contents of your decoded JWT.
+type AuthData = {
+  sub: string | null
+}
+
+export const permissions = definePermissions<AuthData, Schema>(schema, () => {
+  const allowIfLoggedIn = (
+    authData: AuthData,
+    { cmpLit }: ExpressionBuilder<Schema, keyof Schema["tables"]>
+  ) => cmpLit(authData.sub, "IS NOT", null);
+
+  const allowIfMessageSender = (
+    authData: AuthData,
+    { cmp }: ExpressionBuilder<Schema, 'message'>
+  ) => cmp('senderId', '=', authData.sub ?? '')
+
+  return {
+    user: {
+      row: {
+        select: ANYONE_CAN,
+      },
+    },
+    message: {
+      row: {
+        // anyone can insert
+        insert: ANYONE_CAN,
+        update: {
+          // sender can only edit own messages
+          preMutation: [allowIfMessageSender],
+          // sender can only edit messages to be owned by self
+          postMutation: [allowIfMessageSender],
+        },
+        // must be logged in to delete
+        delete: [allowIfLoggedIn],
+        // everyone can read current messages
+        select: ANYONE_CAN,
+      },
+    },
+  } satisfies PermissionsConfig<AuthData, Schema>
 })
