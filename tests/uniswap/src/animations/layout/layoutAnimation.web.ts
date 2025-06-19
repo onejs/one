667010ab33ac2a2c5282696ff8d111a7b5c10e1b
natew <natewienert@gmail.com>
2025-06-19 10:27:30 -1000

    improve llink to add checking of misaligned deps

--- tests/uniswap/src/animations/layout/layoutAnimation.web.ts
+++
@@ -1,6 +0,0 @@
-import { LayoutAnimationOptions } from 'ui/src/animations/layout/types'
-import { NotImplementedError } from 'utilities/src/errors'
-
-export function easeInEaseOutLayoutAnimation(_options?: LayoutAnimationOptions): void {
-  throw new NotImplementedError('easeInEaseOutLayoutAnimation')
-}
--- tests/uniswap/src/loading/types.ts
+++
@@ -1,8 +0,0 @@
-import { ColorTokens } from 'tamagui'
-
-export type SpinningLoaderProps = {
-  size?: number
-  width?: number
-  disabled?: boolean
-  color?: ColorTokens
-}
