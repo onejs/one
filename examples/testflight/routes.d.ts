08f84f1fe167418aa676755a0aa6685ac866ab27
Pokai Chang <mail@zeta.vg>
2025-06-27 22:01:11 +0800

    update auto generated `routes.d.ts` and `tamagui.css` for testflight app

--- examples/testflight/routes.d.ts
+++ examples/testflight/routes.d.ts
@@ -1,17 +1,13 @@
+// deno-lint-ignore-file
+/* eslint-disable */
+// biome-ignore: needed import
+import type { OneRouter } from 'one'
+
 declare module 'one' {
   export namespace OneRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes:
-        | `/`
-        | `/_sitemap`
-        | `/notifications`
-        | `/notifications/`
-        | `/profile`
-        | `/profile/`
-      DynamicRoutes:
-        | `/notifications/post/${OneRouter.SingleRoutePart<T>}`
-        | `/post/${OneRouter.SingleRoutePart<T>}`
-        | `/profile/post/${OneRouter.SingleRoutePart<T>}`
+      StaticRoutes: `/` | `/_sitemap` | `/notifications` | `/notifications/` | `/profile` | `/profile/`
+      DynamicRoutes: `/notifications/post/${OneRouter.SingleRoutePart<T>}` | `/post/${OneRouter.SingleRoutePart<T>}` | `/profile/post/${OneRouter.SingleRoutePart<T>}`
       DynamicRouteTemplate: `/notifications/post/[id]` | `/post/[id]` | `/profile/post/[id]`
       IsTyped: true
     }
