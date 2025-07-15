152ab65e3516bec64c151b9b8076e4bd81930fe1
Pokai Chang <mail@zeta.vg>
2025-07-15 14:52:29 +0800

    add appium util function setValueSafe which is faster than setValueSlowly

--- packages/test/src/utils/appium.ts
+++ packages/test/src/utils/appium.ts
@@ -2,6 +2,8 @@ import type { ChainablePromiseElement, Browser } from 'webdriverio'
 
 /**
  * Like `element.setValue` but types slowly, character by character, to reduce the risk of missing characters.
+ *
+ * See also: `setValueSafe` - it's much faster.
  */
 export async function setValueSlowly(
   driver: Browser,
@@ -32,12 +34,63 @@ export async function setValueSlowly(
 }
 
 /**
+ * Like `element.setValue` but using clipboard paste, to reduce the risk of missing characters.
+ *
+ * See also: `setValueSafe` - it's safer since it does not rely on the clipboard, but is slower.
+ */
+export async function setValueSafe(
+  driver: Browser,
+  element: ChainablePromiseElement,
+  text: string
+) {
+  await driver.setClipboard(Buffer.from(text).toString('base64'), 'plaintext')
+  await element.clearValue()
+  await element.click()
+  await driver.pause(50)
+
+  // Not working on iOS
+  // await driver.executeScript('mobile: paste', [])
+
+  // Not working on iOS
+  // await driver.execute('mobile: performEditorAction', { action: 'paste' })
+
+  // Not working on iOS
+  // await driver.keys(['Meta', 'v']);
+
+  // Not working on iOS
+  // await driver.performActions([
+  //   {
+  //     type: 'key',
+  //     id: 'keyboard',
+  //     actions: [
+  //       { type: 'keyDown', value: '\uE03D' },
+  //       { type: 'keyDown', value: 'v' },
+  //       { type: 'keyUp', value: 'v' },
+  //       { type: 'keyUp', value: '\uE03D' },
+  //     ],
+  //   },
+  // ])
+  // await driver.releaseActions()
+
+  // Works on iOS
+  // https://github.com/appium/appium-xcuitest-driver/blob/v9.9.6/docs/reference/execute-methods.md#mobile-keys
+  await driver.execute('mobile: keys', {
+    keys: [
+      {
+        key: 'v',
+        modifierFlags: 1 << 4, // XCUIKeyModifierCommand
+      },
+    ],
+  })
+}
+
+/**
  * Currently, this only works when the `TestNavigationHelper` component is shown on the screen.
  */
 export async function navigateTo(driver: Browser, path: string) {
   const navigatePathInput = driver.$('~test-navigate-path-input')
   await navigatePathInput.waitForDisplayed({ timeout: 2 * 60 * 1000 })
-  await setValueSlowly(driver, navigatePathInput, path)
+  await setValueSafe(driver, navigatePathInput, path)
   await driver.$('~test-navigate').click()
   await driver.pause(100)
 }
