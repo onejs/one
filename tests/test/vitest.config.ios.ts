fda398e89a4b592c91657b5220b936716c748eea
Pokai Chang <mail@zeta.vg>
2025-07-17 09:18:12 +0800

    ensure native test files won't run in parallel

--- tests/test/vitest.config.ios.ts
+++ tests/test/vitest.config.ios.ts
@@ -8,5 +8,7 @@ export default defineConfig({
     ...defaultConfig.test,
     globalSetup: '@vxrn/test/setup-ios',
     include: ['**/*.{test.\ios,spec.\ios}.?(c|m)[jt]s?(x)'],
+    // Ensure test files won't run in parallel since there is only one simulator device
+    fileParallelism: false,
   },
 })
