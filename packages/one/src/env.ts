734dac9276743c5171f0f8889e8b29bf091f0c50
zetavg <mail@zeta.vg>
2025-07-28 02:25:28 +0800

    ONE_METRO_MODE should be considered as public api

---
+++ packages/one/src/env.ts
@@ -0,0 +1,8 @@
+// Environment variables used here are considered public API, do not change or remove without careful consideration.
+
+/**
+ * Force enabling Metro mode.
+ *
+ * Note: This is considered public API, do not change or remove without careful consideration.
+ */
+export const ONE_METRO_MODE = process.env.ONE_METRO_MODE
