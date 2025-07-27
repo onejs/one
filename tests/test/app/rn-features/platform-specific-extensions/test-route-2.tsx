00c82d75c0b1df997e52e93db3774d50c1e916db
zetavg <mail@zeta.vg>
2025-07-28 02:49:45 +0800

    update component names

--- tests/test/app/rn-features/platform-specific-extensions/test-route-2.tsx
+++ tests/test/app/rn-features/platform-specific-extensions/test-route-2.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import { TestNavigationHelper } from '~/features/test-helpers/TestNavigationHelper'
 
-export default function TestRoute1() {
+export default function TestRoute2() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text testID="test-route-2">test-route-2</Text>
