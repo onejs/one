30fc754657948086398069597e542fa1ed5ae97a
zetavg <mail@zeta.vg>
2025-08-01 22:49:24 +0800

    isolated platform specific extensions on routes between native/web

---
+++ tests/test/app-cases/platform-specific-extensions-for-routes/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Page() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ backgroundColor: 'white', color: 'black' }}>This is index.tsx</Text>
+    </View>
+  )
+}
