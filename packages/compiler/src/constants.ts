8eb076ece806f5423e6d6d4ff10be586fd8d3bf9
natew <natewienert@gmail.com>
2025-08-11 10:51:45 -1000

    feat(one): support importAttributes and using

--- packages/compiler/src/constants.ts
+++ packages/compiler/src/constants.ts
@@ -11,11 +11,21 @@ export const asyncGeneratorRegex = /(async \*|async function\*|for await)/
 export const parsers: Record<string, ParserConfig> = {
   '.tsx': { syntax: 'typescript', tsx: true, decorators: true },
   '.ts': { syntax: 'typescript', tsx: false, decorators: true },
-  '.jsx': { syntax: 'ecmascript', jsx: true },
-  '.js': { syntax: 'ecmascript' },
-  '.mjs': { syntax: 'ecmascript' },
-  '.cjs': { syntax: 'ecmascript' },
-  '.mdx': { syntax: 'ecmascript', jsx: true },
+  '.jsx': {
+    syntax: 'ecmascript',
+    jsx: true,
+    importAttributes: true,
+    explicitResourceManagement: true,
+  },
+  '.js': { syntax: 'ecmascript', importAttributes: true, explicitResourceManagement: true },
+  '.mjs': { syntax: 'ecmascript', importAttributes: true, explicitResourceManagement: true },
+  '.cjs': { syntax: 'ecmascript', importAttributes: true, explicitResourceManagement: true },
+  '.mdx': {
+    syntax: 'ecmascript',
+    jsx: true,
+    importAttributes: true,
+    explicitResourceManagement: true,
+  },
 }
 
 export const validParsers = new Set([...Object.keys(parsers), '.css'])
