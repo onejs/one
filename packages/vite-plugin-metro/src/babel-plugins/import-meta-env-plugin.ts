6f14f1c8b63399dcd43d5bee9eb57e1cf22ba81e
zetavg <mail@zeta.vg>
2025-08-23 01:27:52 +0800

    add comments

--- packages/vite-plugin-metro/src/babel-plugins/import-meta-env-plugin.ts
+++ packages/vite-plugin-metro/src/babel-plugins/import-meta-env-plugin.ts
@@ -6,6 +6,11 @@ type PluginOptions = {
   env?: Record<string, string | undefined>
 }
 
+/**
+ * A Babel plugin to replace `import.meta.env` and `import.meta.env.*` with the provided env variables.
+ *
+ * This plugin may not be referenced directly since metro runs transformers in separate workers, search for `@vxrn/vite-plugin-metro/babel-plugins/import-meta-env-plugin` to see how it's used.
+ */
 export const importMetaEnvPlugin = declare<PluginOptions>((api, options): PluginObj => {
   api.assertVersion(7)
 
