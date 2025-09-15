2d3deec9f4ff0988cae6f26900335027957de811
natew <natewienert@gmail.com>
2025-09-14 17:42:14 -0700

    fix(one): apply same exit fix to dev cli

--- packages/one/src/cli/dev.ts
+++ packages/one/src/cli/dev.ts
@@ -40,7 +40,11 @@ export async function dev(args: {
   })
 
   process.on('SIGINT', () => {
-    stop()
+    try {
+      stop()
+    } finally {
+      process.exit(2)
+    }
   })
 
   process.on('uncaughtException', (err) => {
