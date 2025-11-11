bfb08bd05fc0b7d76e76707d10660083e56452dc
natew <natewienert@gmail.com>
2025-11-11 12:55:37 -1000

    fix(one): fix redirect type to be just Href

--- packages/one/src/utils/redirect.ts
+++ packages/one/src/utils/redirect.ts
@@ -2,11 +2,15 @@ import { getURL } from '../getURL'
 import type { OneRouter } from '../interfaces/router'
 import { router } from '../router/imperative-api'
 
-export const redirect = (path: OneRouter.Href & string, status?: number) => {
+export const redirect = (
+  path: OneRouter.Href extends string ? OneRouter.Href : string,
+  status?: number
+) => {
   if (process.env.VITE_ENVIRONMENT === 'client') {
     router.navigate(path)
     return
   }
 
-  return Response.redirect(path[0] === '/' ? `${getURL()}${path}` : path, status)
+  const finalPath = path[0] === '/' ? `${getURL()}${path}` : path
+  return Response.redirect(finalPath, status)
 }
