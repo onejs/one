f0a6999c7e5072530891bc3b052a093aeff02074
natew <natewienert@gmail.com>
2025-11-18 14:43:52 -1000

    only warn once

--- packages/one/src/getURL.native.ts
+++ packages/one/src/getURL.native.ts
@@ -1,17 +1,22 @@
 import { getDevServer } from './getDevServer'
 
+let hasWarnedOnce = false
+
 export function getURL() {
   let url = process.env.ONE_SERVER_URL
 
   if (__DEV__) {
     // We'll be using the development server in development mode, but we still need to warn the user to set SERVER_URL for things to work in production.
     if (!url) {
-      console.warn(
-        `The ONE_SERVER_URL environment variable is not set. While things will work in development mode as we'll be using your development server, you should still set ONE_SERVER_URL in your .env file for your production builds to work.`
-      )
+      if (hasWarnedOnce) {
+        hasWarnedOnce = true
+        console.warn(
+          `The ONE_SERVER_URL environment variable is not set. While things will work in development mode as we'll be using your development server, you should still set ONE_SERVER_URL in your .env file for your production builds to work.`
+        )
+      }
     }
 
-    const { url: devServerUrl } = getDevServer() as { url: string }
+    const { url: devServerUrl } = getDevServer()
     url = devServerUrl
   }
 
