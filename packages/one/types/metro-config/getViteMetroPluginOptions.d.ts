ad5a2fa854b371f6a14080c4afa3c68c91607c9c
natew <natewienert@gmail.com>
2025-12-10 11:43:03 -0600

    fix build

--- packages/one/types/metro-config/getViteMetroPluginOptions.d.ts
+++ packages/one/types/metro-config/getViteMetroPluginOptions.d.ts
@@ -1,8 +1,13 @@
 import type { metroPlugin } from '@vxrn/vite-plugin-metro';
-export declare function getViteMetroPluginOptions({ projectRoot, relativeRouterRoot, ignoredRouteFiles, userDefaultConfigOverrides, }: {
+export declare function getViteMetroPluginOptions({ projectRoot, relativeRouterRoot, ignoredRouteFiles, userDefaultConfigOverrides, setupFile, }: {
     projectRoot: string;
     relativeRouterRoot: string;
     ignoredRouteFiles?: Array<`**/*${string}`>;
     userDefaultConfigOverrides?: NonNullable<Parameters<typeof metroPlugin>[0]>['defaultConfigOverrides'];
+    setupFile?: string | {
+        native?: string;
+        ios?: string;
+        android?: string;
+    };
 }): Parameters<typeof metroPlugin>[0];
 //# sourceMappingURL=getViteMetroPluginOptions.d.ts.map
