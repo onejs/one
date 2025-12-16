e9302b6b1a41d5a3ed8db35bdcfa9685b2a6c238
natew <natewienert@gmail.com>
2025-12-16 10:42:52 -0600

    add missing types

---
+++ packages/vite-native-hmr/types/hmr-client.d.ts
@@ -0,0 +1,34 @@
+/**
+ * 🚨🚨🚨🚨🚨🚨🚨
+ *
+ *
+ * It seems we're doing HMR from `@vxrn/vite-native-client`
+ *
+ *
+ * Can we remove this?
+ *
+ */
+/**
+ * Represent Hot Module Replacement Update body.
+ *
+ * @internal
+ */
+export interface HMRMessageBody {
+    name: string;
+    time: number;
+    hash: string;
+    warnings: any[];
+    errors: any[];
+    modules: Record<string, string>;
+}
+/**
+ * Represent Hot Module Replacement Update message.
+ *
+ * @internal
+ */
+export interface HMRMessage {
+    action: 'building' | 'built' | 'sync';
+    body: HMRMessageBody | null;
+}
+export declare const loadHMRClient: () => void;
+//# sourceMappingURL=hmr-client.d.ts.map
