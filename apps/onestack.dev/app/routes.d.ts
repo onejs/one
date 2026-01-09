5715b668faeb3a9f224db9df848cbf95b32554c1
natew <natewienert@gmail.com>
2026-01-08 17:35:45 -1000

    reformat routes

--- apps/onestack.dev/app/routes.d.ts
+++ apps/onestack.dev/app/routes.d.ts
@@ -6,25 +6,9 @@ import type { OneRouter } from 'one'
 declare module 'one' {
   export namespace OneRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes:
-        | `/`
-        | `/(content)`
-        | `/(content)/blog`
-        | `/(content)/docs`
-        | `/_sitemap`
-        | `/blog`
-        | `/docs`
-        | `/test`
-      DynamicRoutes:
-        | `/(content)/blog/${OneRouter.SingleRoutePart<T>}`
-        | `/(content)/docs/${OneRouter.SingleRoutePart<T>}`
-        | `/blog/${OneRouter.SingleRoutePart<T>}`
-        | `/docs/${OneRouter.SingleRoutePart<T>}`
-      DynamicRouteTemplate:
-        | `/(content)/blog/[slug]`
-        | `/(content)/docs/[slug]`
-        | `/blog/[slug]`
-        | `/docs/[slug]`
+      StaticRoutes: `/` | `/(content)` | `/(content)/blog` | `/(content)/docs` | `/_sitemap` | `/blog` | `/docs` | `/test`
+      DynamicRoutes: `/(content)/blog/${OneRouter.SingleRoutePart<T>}` | `/(content)/docs/${OneRouter.SingleRoutePart<T>}` | `/blog/${OneRouter.SingleRoutePart<T>}` | `/docs/${OneRouter.SingleRoutePart<T>}`
+      DynamicRouteTemplate: `/(content)/blog/[slug]` | `/(content)/docs/[slug]` | `/blog/[slug]` | `/docs/[slug]`
       IsTyped: true
       RouteTypes: {
         '/(content)/blog/[slug]': RouteInfo<{ slug: string }>
