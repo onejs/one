306563910610ad296b1eb201458c50fd11ecd8fa
DaveyEke <daveydenco@gmail.com>
2026-01-29 18:47:22 +0100

    fix: link to blog and fix typo in blog post

--- apps/onestack.dev/app/index.tsx
+++ apps/onestack.dev/app/index.tsx
@@ -87,6 +87,29 @@ export default function HomePage() {
             <XStack ai="center" gap="$6" $sm={{ jc: 'center' }}>
               <ToggleThemeButton />
 
+              <Link href="/blog" asChild>
+                <Button
+                  size="$5"
+                  bg="$color4"
+                  br="$10"
+                  group
+                  animation="quickest"
+                  containerType="normal"
+                  gap={0}
+                  bw={0}
+                  hoverStyle={{
+                    bg: '$color5',
+                  }}
+                  pressStyle={{
+                    bg: '$color6',
+                  }}
+                >
+                  <ButtonText color="$color12" fontFamily="$mono" lh={0}>
+                    Status
+                  </ButtonText>
+                </Button>
+              </Link>
+
               <Theme name="accent">
                 <Link href="/docs/introduction" asChild>
                   <Button
