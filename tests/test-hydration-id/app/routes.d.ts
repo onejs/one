64f02979e15ef565f7b667d8119312f137808c2a
natew <natewienert@gmail.com>
2026-02-03 07:16:37 -1000

    fix intercept-test route to be SPA for prod build

--- tests/test-hydration-id/app/routes.d.ts
+++ tests/test-hydration-id/app/routes.d.ts
@@ -6,7 +6,11 @@ import type { OneRouter } from 'one'
 declare module 'one' {
   export namespace OneRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/_sitemap` | `/nested` | `/nested/page`
+      StaticRoutes: 
+        | `/`
+        | `/_sitemap`
+        | `/nested`
+        | `/nested/page`
       DynamicRoutes: never
       DynamicRouteTemplate: never
       IsTyped: true
