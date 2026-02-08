0da53e2086b157fe81167beb64603bfb267c2644
natew <natewienert@gmail.com>
2026-02-07 21:42:10 -1000

    upgrade tamagui, fix native test

--- tests/test/tests/basic.test.ios.ts
+++ tests/test/tests/basic.test.ios.ts
@@ -7,5 +7,6 @@ test('basic iOS test', sharedTestOptions, async () => {
   const driver = await remote(getWebDriverConfig())
 
   const textElementInComponent = await driver.$(`~welcome-message`)
+  await textElementInComponent.waitForDisplayed({ timeout: 2 * 60 * 1000 })
   expect(await textElementInComponent.getText()).toBe('Welcome to One')
 })
