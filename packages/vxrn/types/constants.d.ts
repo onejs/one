7852db78b71cbb36671936c8e1b60a652b52afd9
natew <natewienert@gmail.com>
2026-04-10 07:29:32 -1000

    canary1.15.8-1775842153357

--- packages/vxrn/types/constants.d.ts
+++ packages/vxrn/types/constants.d.ts
@@ -1,4 +1,6 @@
 export declare const DEFAULT_PORT = 8081;
 export declare const webExtensions: string[];
 export declare const ssrExtensions: string[];
+export declare const iosExtensions: string[];
+export declare const androidExtensions: string[];
 //# sourceMappingURL=constants.d.ts.map
