b44177556c35e2c0c3adcbfa709ec6f2f089afe2
natew <natewienert@gmail.com>
2026-05-07 11:06:19 -1000

    v1.16.10

--- packages/one/types/utils/isStatus.d.ts
+++ packages/one/types/utils/isStatus.d.ts
@@ -1,3 +1,4 @@
 import type { RedirectStatusCode } from 'hono/utils/http-status';
-export declare function isStatusRedirect(status: number): status is RedirectStatusCode;
+export type LocationRedirectStatusCode = Extract<RedirectStatusCode, 301 | 302 | 303 | 307 | 308>;
+export declare function isStatusRedirect(status: number): status is LocationRedirectStatusCode;
 //# sourceMappingURL=isStatus.d.ts.map
