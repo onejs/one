55ec056725c10a94741292c1d4063fafb2226a10
natew <natewienert@gmail.com>
2026-05-21 07:44:48 -1000

    chore(one): add build output pointer declarations
    Add generated declaration files for the build output pointer utility introduced by the serve outDir fix.

---
+++ packages/one/types/utils/buildOutputPointer.d.ts
@@ -0,0 +1,4 @@
+export declare const buildOutputPointerPath: string;
+export declare function writeBuildOutputPointer(outDir: string): Promise<void>;
+export declare function resolveServeOutDir(outDir?: string): Promise<string>;
+//# sourceMappingURL=buildOutputPointer.d.ts.map
---
+++ packages/one/types/utils/buildOutputPointer.test.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=buildOutputPointer.test.d.ts.map
