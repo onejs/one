67c410b2b2a76223ee1458694e33216686a95c10
natew <natewienert@gmail.com>
2026-09-20 21:37:18 -1000

    sync MenuAction declaration path
    Team-Machine-Session: r42922

--- packages/native/types/extras.d.ts
+++ packages/native/types/extras.d.ts
@@ -3,8 +3,8 @@ export type { ColorType } from './color';
 export { ZoomTransitionSource, ZoomTransitionEnabler, ZoomTransitionAlignmentRectDetector, } from './zoom';
 export { ToolbarHost, ToolbarItem } from './toolbar';
 export type { ToolbarHostProps, ToolbarItemProps } from './toolbar';
-export { MenuAction } from './menu';
-export type { MenuActionProps } from './menu';
+export { MenuAction } from './menu/index';
+export type { MenuActionProps } from './menu/index';
 export { SplitView } from './split-view';
 export type { SplitViewProps, SplitViewColumnProps } from './split-view';
 //# sourceMappingURL=extras.d.ts.map
