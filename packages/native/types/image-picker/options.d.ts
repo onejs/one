fd4d2d50886d736b9fd5f798bac62cbbb82aa9f8
natew <natewienert@gmail.com>
2026-09-21 11:20:59 -1000

    one-native-image-picker: regen options.d.ts after guard removal
    Team-Machine-Session: m16066

--- packages/native/types/image-picker/options.d.ts
+++ packages/native/types/image-picker/options.d.ts
@@ -5,5 +5,4 @@ export interface ResolvedImagePickerOptions {
 }
 export declare function resolveImagePickerOptions(options?: ImagePickerOptions): ResolvedImagePickerOptions;
 export declare function resolveCameraOptions(options?: ImagePickerOptions): ResolvedImagePickerOptions;
-export declare function createRequestGuard(): <T>(verb: string, run: () => Promise<T>) => Promise<T>;
 //# sourceMappingURL=options.d.ts.map
