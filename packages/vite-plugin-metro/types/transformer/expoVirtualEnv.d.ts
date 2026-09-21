62f1bc41c48b428c4eaef6ba76553e05b736798d
natew <natewienert@gmail.com>
2026-09-20 21:37:48 -1000

    publish the Expo virtual env transformer type
    Team-Machine-Session: r42922

---
+++ packages/vite-plugin-metro/types/transformer/expoVirtualEnv.d.ts
@@ -0,0 +1,8 @@
+export declare function substituteExpoVirtualEnvSource({ filename, src, projectRoot, dev, environment, }: {
+    filename: string;
+    src: string;
+    projectRoot: string;
+    dev: boolean | undefined;
+    environment: unknown;
+}): string;
+//# sourceMappingURL=expoVirtualEnv.d.ts.map
