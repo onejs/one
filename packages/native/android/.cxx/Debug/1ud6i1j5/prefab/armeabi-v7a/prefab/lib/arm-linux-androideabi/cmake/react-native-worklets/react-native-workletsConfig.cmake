83e49d25bdb02607b78ec4f48f9c44eccc30edfb
natew <natewienert@gmail.com>
2026-09-22 14:32:32 -1000

    preserve: ownerless dirt untouched 24h+ (one-native-assembled)
    worktree: /Users/n8/.worktrees/one-native-assembled
    files: 165
    skipped-over-10MB: none

---
+++ packages/native/android/.cxx/Debug/1ud6i1j5/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-worklets/react-native-workletsConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-worklets::worklets)
+add_library(react-native-worklets::worklets SHARED IMPORTED)
+set_target_properties(react-native-worklets::worklets PROPERTIES
+    IMPORTED_LOCATION "/Users/n8/.worktrees/one-native-assembled/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/r2mj4145/obj/armeabi-v7a/libworklets.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/n8/.worktrees/one-native-assembled/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
