3e568aaf3df10009e2f85ae2a1a1659cc9e3bbbe
natew <natewienert@gmail.com>
2026-09-22 15:15:20 -1000

    Autolink One bundled native modules
    Team-Machine-Session: p49927

--- packages/one/react-native-config.cjs
+++ packages/one/react-native-config.cjs
@@ -1,10 +1,23 @@
 const path = require('node:path')
+const nativeRoot = path.dirname(require.resolve('@vxrn/native/package.json'))
+const bundledNativePackages = [
+  '@op-engineering/op-sqlite',
+  'react-native-nitro-image',
+  'react-native-nitro-modules',
+  'react-native-nitro-web-image',
+]
 
 module.exports = {
   commands: [...require('vxrn/react-native-commands')],
   dependencies: {
     '@vxrn/native': {
-      root: path.dirname(require.resolve('@vxrn/native/package.json')),
+      root: nativeRoot,
     },
+    ...Object.fromEntries(
+      bundledNativePackages.map((name) => [
+        name,
+        { root: path.dirname(require.resolve(`${name}/package.json`, { paths: [nativeRoot] })) },
+      ])
+    ),
   },
 }
