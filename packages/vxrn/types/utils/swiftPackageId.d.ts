9e7a0650e7265c1a3693c1c985d73d88d7ecee4d
natew <natewienert@gmail.com>
2026-09-26 05:28:21 -1000

    chore(vxrn): expose Swift package directory types to consumers

--- packages/vxrn/types/utils/swiftPackageId.d.ts
+++ packages/vxrn/types/utils/swiftPackageId.d.ts
@@ -1,3 +1,4 @@
 export declare function swiftPackageId(packageDir: string): string;
 export declare function swiftPackageDirOf(file: string): string | undefined;
+export declare function swiftPackageDirectories(root: string): Map<string, string>;
 //# sourceMappingURL=swiftPackageId.d.ts.map
